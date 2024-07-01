00a5e459785e4f48f51a387dbf3fef3c54896365
Philipp Vogel <philipp.vogel@nuuone.de>
2024-07-01 09:11:51 +0200

    feat: add ui components

---
+++ frontend/src/lib/utils.ts
@@ -0,0 +1,20 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export const genRandomTree = (N = 9000, reverse = false) => {
+  return {
+    nodes: [...Array(N).keys()].map((i) => ({ id: i })),
+    links: [...Array(N).keys()]
+      .filter((id) => id)
+      .map((id) => ({
+        [reverse ? "target" : "source"]: id,
+        [reverse ? "source" : "target"]: Math.round(Math.random() * (id - 1)),
+      })),
+  };
+};
+
+export const deepCopyData = (data: any) => JSON.parse(JSON.stringify(data));
