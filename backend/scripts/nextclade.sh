81ccee04e0641e4ed31ef6a904c14ed219ff10ba
Philipp Vogel <philipp.vogel@nuuone.de>
2024-07-03 17:52:30 +0200

    fix(docker api): delete mounting for dev to prevent overwritting the app directory

--- backend/scripts/nextclade.sh
+++ backend/scripts/nextclade.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# activate conda environment
-source /home/ubuntu/miniconda3/bin/activate num-dash
-
 # run nextclade
 nextclade run $1 --output-json $2 --input-dataset datasets/nextclade_covid
