632f0bfb80f3d9442a72e625e74cfa16585cb778
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2024-07-04 16:38:06 +0200

    chore: adjust source text style

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -4,8 +4,8 @@ export const Footer = () => {
             <div className="flex justify-between items-center p-4 max-w-[1500px] mx-auto text-sm">
                 <div>
                     <div>
-                        Made with &#9829; in Düsseldorf by{" "}
-                        <a href="https://www.medmikrobio.hhu.de/ag-dilthey" className="font-medium" target="_blank">
+                        Made with <span className="text-primary">&#9829;</span> in Düsseldorf by&nbsp;
+                        <a href="https://www.medmikrobio.hhu.de/ag-dilthey" className="font-bold" target="_blank">
                             DiltheyLab
                         </a>
                     </div>
