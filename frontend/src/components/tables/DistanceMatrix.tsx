507d40b8e462fca12ee509accebb7ab8146b6642
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2024-07-04 09:52:14 +0200

    Refactor distance matrix component

--- frontend/src/components/tables/DistanceMatrix.tsx
+++ frontend/src/components/tables/DistanceMatrix.tsx
@@ -6,6 +6,7 @@ export function DistanceMatrix() {
     const [matrixData, setMatrixData] = useState<DistanceMatrixSchema | undefined>();
     const [hoveredRow, setHoveredRow] = useState<number | undefined>();
     const [hoveredColumn, setHoveredColumn] = useState<number | undefined>();
+
     // how to listen for new data? context?
     useEffect(() => {
         const loadMatrixData = async () => {
@@ -14,18 +15,61 @@ export function DistanceMatrix() {
         loadMatrixData();
     }, []);
 
+    const renderColumn = (cellValue: number, colIndex: number, rowIndex: number) => {
+        return (
+            <TableCell
+                key={colIndex}
+                className={`border-r-[1px] border-muted p-2 text-center text-xs ${
+                    hoveredColumn === colIndex && hoveredRow === rowIndex
+                        ? "bg-muted"
+                        : hoveredColumn === colIndex
+                        ? "bg-muted/30"
+                        : null
+                }`}
+                onMouseEnter={() => setHoveredColumn(colIndex)}
+                onMouseLeave={() => setHoveredColumn(undefined)}
+            >
+                {cellValue}
+            </TableCell>
+        );
+    };
+
+    const renderRows = () => {
+        if (matrixData) {
+            return matrixData.matrix.map((row, rowIndex) => {
+                return (
+                    <TableRow
+                        key={rowIndex}
+                        className="border-b-[1px] border-r-[1px] border-muted p-2"
+                        onMouseEnter={() => setHoveredRow(rowIndex)}
+                        onMouseLeave={() => setHoveredRow(undefined)}
+                    >
+                        <TableCell
+                            className={`border-r-[1px] border-muted font-medium p-2 text-center text-xs ${
+                                hoveredRow === rowIndex ? "bg-muted" : "bg-muted/30"
+                            }`}
+                        >
+                            {matrixData.row_column_names[rowIndex]}
+                        </TableCell>
+                        {row.map((cellValue, colIndex) => renderColumn(cellValue, colIndex, rowIndex))}
+                    </TableRow>
+                );
+            });
+        }
+    };
+
     return (
         <>
             {matrixData && (
                 <Table className="rounded-xl overflow-hidden border-b-[1px] border-muted">
                     <TableBody>
                         <TableRow className="bg-muted/30 border-r-[1px] border-muted p-2">
-                            <TableCell className="border-r-[1px] border-muted font-medium aspect-square"></TableCell>
+                            <TableCell className="border-r-[1px] border-muted font-medium"></TableCell>
                             {matrixData.row_column_names.map((name, index) => (
                                 <TableCell
                                     key={index}
                                     style={{ writingMode: "vertical-rl" }}
-                                    className={`border-r-[1px] border-muted p-2 font-medium text-center text-xs aspect-spare rotate-180 ${
+                                    className={`border-r-[1px] border-muted p-2 font-medium text-center text-xs rotate-180 ${
                                         hoveredColumn === index ? "bg-muted" : "bg-muted/30"
                                     }`}
                                 >
@@ -33,38 +77,7 @@ export function DistanceMatrix() {
                                 </TableCell>
                             ))}
                         </TableRow>
-                        {matrixData.matrix.map((row, rowIndex) => {
-                            return (
-                                <TableRow
-                                    key={rowIndex}
-                                    className="border-b-[1px] border-r-[1px] border-muted p-2"
-                                    onMouseEnter={() => setHoveredRow(rowIndex)}
-                                >
-                                    <TableCell
-                                        className={`border-r-[1px] border-muted font-medium p-2 text-center text-xs aspect-square ${
-                                            hoveredRow === rowIndex ? "bg-muted" : "bg-muted/30"
-                                        }`}
-                                    >
-                                        {matrixData.row_column_names[rowIndex]}
-                                    </TableCell>
-                                    {row.map((col, colIndex) => (
-                                        <TableCell
-                                            key={colIndex}
-                                            className={`border-r-[1px] border-muted p-2 text-center text-xs aspect-square ${
-                                                hoveredColumn === colIndex && hoveredRow === rowIndex
-                                                    ? "bg-muted"
-                                                    : hoveredColumn === colIndex
-                                                    ? "bg-muted/30"
-                                                    : null
-                                            }`}
-                                            onMouseEnter={() => setHoveredColumn(colIndex)}
-                                        >
-                                            {col}
-                                        </TableCell>
-                                    ))}
-                                </TableRow>
-                            );
-                        })}
+                        {renderRows()}
                     </TableBody>
                 </Table>
             )}
