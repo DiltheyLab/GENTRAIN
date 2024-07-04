f244fe197823376757e5d68d49d69858aab19962
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2024-07-04 14:40:06 +0200

    feature: add layout and footer

---
+++ frontend/src/components/layout/Footer.tsx
@@ -0,0 +1,28 @@
+export const Footer = () => {
+    return (
+        <footer className="mt-5 bg-accent">
+            <div className="flex justify-between items-center p-4 max-w-[1500px] mx-auto text-sm">
+                <div>
+                    <div>
+                        Made with &#9829; in Düsseldorf by{" "}
+                        <a href="https://www.medmikrobio.hhu.de/ag-dilthey" className="font-medium" target="_blank">
+                            DiltheyLab
+                        </a>
+                    </div>
+                    <small>© 2024 Heinrich-Heine-Universität Düsseldorf</small>
+                </div>
+                <div>
+                    <a href="/contact" className="underline ml-4">
+                        Contact
+                    </a>
+                    <a href="/contact" className="underline ml-4">
+                        Datenschutz
+                    </a>
+                    <a href="/contact" className="underline ml-4">
+                        Impressum
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
+};
