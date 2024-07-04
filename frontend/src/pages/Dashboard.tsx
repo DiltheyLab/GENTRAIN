77ee008256bcd0aa934976cefba70271edebad8e
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2024-07-04 11:21:24 +0200

    chore: adjust dashboard layout and max container width

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -9,21 +9,21 @@ export function Dashboard() {
     return (
         <GraphSettingsProvider>
             <Header /> {/* 👈 Es muss noch eine Layout Komponente angelegt und alle Seiten darin gewrappt werden */}
-            <main className="relative mx-auto">
-                <div className="flex p-4 flex-row">
+            <main className="relative max-w-[1500px] mx-auto p-4">
+                <div className="flex flex-row">
                     <div className="w-1/2 md:w-1/3 lg:w-1/6 mr-4">
                         <DashboardGraphSettings />
                     </div>
                     <div className="w-1/2 md:w-2/3 lg:w-5/6">
-                        <div>
-                            <DashboardVisualizationPanel />
-                        </div>
-                        <div className="mt-4">
-                            <DistanceMatrixTable />
-                        </div>
-                        <div className="mt-4">
-                            <SampleInformationTable />
-                        </div>
+                        <DashboardVisualizationPanel />
+                    </div>
+                </div>
+                <div>
+                    <div className="mt-4">
+                        <DistanceMatrixTable />
+                    </div>
+                    <div className="mt-4">
+                        <SampleInformationTable />
                     </div>
                 </div>
             </main>
