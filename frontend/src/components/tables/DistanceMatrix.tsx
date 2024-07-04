48d74c60250c5a083ab8539846651828ce84b750
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2024-07-04 09:28:30 +0200

    Adjust table styling and implement hover highlighting

--- frontend/src/components/tables/DistanceMatrix.tsx
+++ frontend/src/components/tables/DistanceMatrix.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 
 export function DistanceMatrix() {
     const [matrixData, setMatrixData] = useState<DistanceMatrixSchema | undefined>();
+    const [hoveredRow, setHoveredRow] = useState<number | undefined>();
+    const [hoveredColumn, setHoveredColumn] = useState<number | undefined>();
     // how to listen for new data? context?
     useEffect(() => {
         const loadMatrixData = async () => {
@@ -11,27 +13,52 @@ export function DistanceMatrix() {
         };
         loadMatrixData();
     }, []);
+
     return (
         <>
             {matrixData && (
-                <Table className="rounded-xl overflow-hidden">
+                <Table className="rounded-xl overflow-hidden border-b-[1px] border-muted">
                     <TableBody>
-                        <TableRow className="bg-muted">
-                            <TableCell className="font-medium"></TableCell>
+                        <TableRow className="bg-muted/30 border-r-[1px] border-muted p-2">
+                            <TableCell className="border-r-[1px] border-muted font-medium aspect-square"></TableCell>
                             {matrixData.row_column_names.map((name, index) => (
-                                <TableCell key={index} className="font-medium p-2 text-center">
+                                <TableCell
+                                    key={index}
+                                    style={{ writingMode: "vertical-rl" }}
+                                    className={`border-r-[1px] border-muted p-2 font-medium text-center text-xs aspect-spare rotate-180 ${
+                                        hoveredColumn === index ? "bg-muted" : "bg-muted/30"
+                                    }`}
+                                >
                                     {name}
                                 </TableCell>
                             ))}
                         </TableRow>
-                        {matrixData.matrix.map((row, index) => {
+                        {matrixData.matrix.map((row, rowIndex) => {
                             return (
-                                <TableRow key={index}>
-                                    <TableCell className="font-medium bg-muted p-2 text-center">
-                                        {matrixData.row_column_names[index]}
+                                <TableRow
+                                    key={rowIndex}
+                                    className="border-b-[1px] border-r-[1px] border-muted p-2"
+                                    onMouseEnter={() => setHoveredRow(rowIndex)}
+                                >
+                                    <TableCell
+                                        className={`border-r-[1px] border-muted font-medium p-2 text-center text-xs aspect-square ${
+                                            hoveredRow === rowIndex ? "bg-muted" : "bg-muted/30"
+                                        }`}
+                                    >
+                                        {matrixData.row_column_names[rowIndex]}
                                     </TableCell>
-                                    {row.map((col, index) => (
-                                        <TableCell key={index} className="border-r-[1px] p-2 text-center">
+                                    {row.map((col, colIndex) => (
+                                        <TableCell
+                                            key={colIndex}
+                                            className={`border-r-[1px] border-muted p-2 text-center text-xs aspect-square ${
+                                                hoveredColumn === colIndex && hoveredRow === rowIndex
+                                                    ? "bg-muted"
+                                                    : hoveredColumn === colIndex
+                                                    ? "bg-muted/30"
+                                                    : null
+                                            }`}
+                                            onMouseEnter={() => setHoveredColumn(colIndex)}
+                                        >
                                             {col}
                                         </TableCell>
                                     ))}
