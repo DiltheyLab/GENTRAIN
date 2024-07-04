5af67e837bd6e21c45a064a08e691ff32059513b
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2024-07-04 14:16:39 +0200

    chore: highlight text when hovering over matrix values

--- frontend/src/components/tables/DistanceMatrixTable.tsx
+++ frontend/src/components/tables/DistanceMatrixTable.tsx
@@ -1,11 +1,34 @@
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 import { useDistanceMatrixGetById } from "@/database/distance_matrix";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { callbackify } from "util";
 
 export function DistanceMatrixTable() {
     const matrixData = useDistanceMatrixGetById("dm_full");
     const [hoveredRow, setHoveredRow] = useState<number | undefined>();
     const [hoveredColumn, setHoveredColumn] = useState<number | undefined>();
+    const [maxValue, setMaxValue] = useState<number>(0);
+
+    useEffect(() => {
+        let currentMaxValue = 0;
+        matrixData?.matrix?.forEach((row) => {
+            row.forEach((cellValue) => {
+                if (cellValue > currentMaxValue) {
+                    currentMaxValue = cellValue;
+                }
+            });
+        });
+        setMaxValue(currentMaxValue);
+    }, [matrixData?.matrix]);
+
+    // maps the value interval [min, max] onto [0,100] to colorized cell background based on their relative values
+    // results need to be projected to highlight the lower value area (tan, log, exp or something)
+    const getBackgroundOpacityBasedOnValueRange = (value: number) => {
+        if (value <= 0) {
+            return 0;
+        }
+        return Math.round(100 + (-100 / maxValue) * value) / 100;
+    };
 
     const renderColumn = (cellValue: number, colIndex: number, rowIndex: number) => {
         return (
@@ -13,13 +36,19 @@ export function DistanceMatrixTable() {
                 key={colIndex}
                 className={`[&:not(:last-child)]:border-r-[1px] border-muted p-2 text-center text-xs ${
                     hoveredColumn === colIndex && hoveredRow === rowIndex
-                        ? "bg-muted"
+                        ? "bg-muted font-bold"
                         : hoveredColumn === colIndex
-                        ? "bg-muted/30"
+                        ? "bg-muted/20"
                         : null
                 }`}
-                onMouseEnter={() => setHoveredColumn(colIndex)}
-                onMouseLeave={() => setHoveredColumn(undefined)}
+                onMouseEnter={() => {
+                    setHoveredColumn(colIndex);
+                    setHoveredRow(rowIndex);
+                }}
+                onMouseLeave={() => {
+                    setHoveredColumn(undefined);
+                    setHoveredRow(undefined);
+                }}
             >
                 {cellValue}
             </TableCell>
@@ -30,20 +59,17 @@ export function DistanceMatrixTable() {
         if (matrixData) {
             return matrixData.matrix.map((row, rowIndex) => {
                 return (
-                    <TableRow
-                        key={rowIndex}
-                        className="border-b-[1px] border-muted p-2"
-                        onMouseEnter={() => setHoveredRow(rowIndex)}
-                        onMouseLeave={() => setHoveredRow(undefined)}
-                    >
+                    <TableRow key={rowIndex} className="border-b-[1px] border-muted p-2">
                         <TableCell
-                            className={`[&:not(:last-child)]:border-r-[1px] border-muted font-medium p-2 text-center text-xs ${
-                                hoveredRow === rowIndex ? "bg-muted" : "bg-muted/30"
+                            className={`[&:not(:last-child)]:border-r-[1px] border-muted font-medium p-2 text-center text-xs w-[100px] ${
+                                hoveredRow === rowIndex ? "bg-muted font-bold" : "bg-muted/20"
                             }`}
                         >
                             {matrixData.row_column_names[rowIndex]}
                         </TableCell>
-                        {row.map((cellValue, colIndex) => renderColumn(cellValue, colIndex, rowIndex))}
+                        {row.map((cellValue, colIndex) => {
+                            return renderColumn(cellValue, colIndex, rowIndex);
+                        })}
                     </TableRow>
                 );
             });
@@ -64,8 +90,8 @@ export function DistanceMatrixTable() {
                                         <TableCell
                                             key={index}
                                             style={{ writingMode: "vertical-rl" }}
-                                            className={`[&:not(:last-child)]:border-r-[1px] border-muted p-2 font-medium text-center text-xs rotate-180 ${
-                                                hoveredColumn === index ? "bg-muted" : "bg-muted/30"
+                                            className={`[&:not(:last-child)]:border-r-[1px] border-muted p-2 font-medium text-center text-xs rotate-180 h-[100px] ${
+                                                hoveredColumn === index ? "bg-muted font-bold" : "bg-muted/20"
                                             }`}
                                         >
                                             {name}
