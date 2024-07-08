d29b3e57e8a64cd76ec0c6b08c42b805098a09a4
Philipp Vogel <philipp.vogel@nuuone.de>
2024-07-08 10:50:39 +0200

    feat: add hook to fetch matrix and sampling data together

--- frontend/src/components/DashboardVisualizationPanel.tsx
+++ frontend/src/components/DashboardVisualizationPanel.tsx
@@ -6,13 +6,11 @@ import { ForcedDirectedGraph3D } from "./graphs/ForcedDirectedGraph3D";
 import { useEffect, useLayoutEffect, useMemo, useRef, useState } from "react";
 import { deepCopyData } from "@/lib/utils";
 import { transformMatrixToGraphData } from "@/services/graphs";
-import { useDistanceMatrixGetById } from "@/database/distance_matrix";
-import { useSamplesGetAll } from "@/database/samples";
+import { useDistanceMatrixAndSamplesById } from "@/database/distance_matrix";
 
 export const DashboardVisualizationPanel = () => {
     const graphSettingsContext = useGraphSettings();
-    const matrixData = useDistanceMatrixGetById("dm_full");
-    const samples = useSamplesGetAll();
+    const matrixDataWithMetaData = useDistanceMatrixAndSamplesById("dm_full");
 
     //get size of parent container
     const [height, setHeight] = useState(0);
@@ -20,12 +18,15 @@ export const DashboardVisualizationPanel = () => {
     const containerRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        if (!matrixData || !samples || !graphSettingsContext) return;
-        const graphData = transformMatrixToGraphData(matrixData, samples);
+        if (!matrixDataWithMetaData || !matrixDataWithMetaData.distanceMatrix || !graphSettingsContext) return;
+        const graphData = transformMatrixToGraphData(
+            matrixDataWithMetaData.distanceMatrix,
+            matrixDataWithMetaData.samples
+        );
 
         // Update the graph settings with the new graph data
         graphSettingsContext.updateSettings({ graphData });
-    }, [matrixData, samples]);
+    }, [matrixDataWithMetaData]);
 
     useLayoutEffect(() => {
         if (!containerRef.current) return;
@@ -37,6 +38,7 @@ export const DashboardVisualizationPanel = () => {
         const onResize = () => {
             if (!containerRef.current) return;
             setWidth(containerRef.current.offsetWidth - 8);
+            setHeight(containerRef.current.offsetHeight);
         };
         window.addEventListener("resize", onResize);
         return () => {
