02c7f9fbcfb326093b71994f08db7580c97c7642
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2024-07-17 08:12:17 +0200

    chore: toggle pathogen select on every page if not selected

--- frontend/src/pages/DataUpload.tsx
+++ frontend/src/pages/DataUpload.tsx
@@ -5,7 +5,6 @@ import { columns } from "@/components/tables/cases/columns";
 import { Separator } from "@/components/ui/separator";
 import { UploadSection } from "@/components/dataUpload/UploadSection";
 import { useGetAllCases } from "@/hooks/database/cases/useGetAllCases";
-import { PathogenDialog } from "@/components/dataUpload/PathogenDialog";
 
 export function DataUpload() {
     const casesData = useGetAllCases();
@@ -37,7 +36,6 @@ export function DataUpload() {
                     {casesData && <DataTable data={casesData} columns={columns} />}
                 </div>
             </div>
-            <PathogenDialog />
         </Layout>
     );
 }
