c044d679e51a79752f609403f142f289fc2c374f
Philipp Vogel <philipp.vogel@nuuone.de>
2024-07-18 11:11:06 +0200

    feat: create CRUD operations for analyses

--- frontend/src/database/analyses.ts
+++ frontend/src/database/analyses.ts
@@ -1,27 +1,29 @@
-import { analysisSettings } from "@/stores/analysis";
-import { z } from "zod";
+import { AnalysisSettings } from "@/stores/analysis";
 import { db } from "./db";
-
 export interface AnalysisSchema {
     id: number;
     name: string;
-    config: analysisSettings;
+    settings: AnalysisSettings;
     created_at?: Date;
     updated_at?: Date;
 }
 
-export const analysisRules = z.object({
-    name: z.string().min(1),
-    sample_id: z.string().min(1).or(z.null()),
-    config: z.object({
-        nodeSize: z.number(),
-        linkWidth: z.number(),
-        zoomToFit: z.boolean(),
-        charge: z.number(),
-        linkDistance: z.number(),
-    }),
-});
-
 export const getAllAnalyses = () => {
     return db.analyses.toArray();
+};
+
+export const createAnalysis = async (name: string, settings: AnalysisSettings) => {
+    const analysis = {
+        name: name,
+        settings: settings,
+    };
+    return await db.analyses.add(analysis);
+};
+
+export const getAnalysisByID = (id: number) => {
+    return db.analyses.get(id);
+};
+
+export const updateAnalysisSettings = async (id: number, settings: AnalysisSettings) => {
+    return await db.analyses.update(id, { settings: settings });
 };
