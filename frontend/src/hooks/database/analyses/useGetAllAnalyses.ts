63fc9245c27f22ff26d2d49b53c89c524b833e07
Philipp Vogel <philipp.vogel@nuuone.de>
2024-07-18 11:12:12 +0200

    feat: add hooks for fetching analysis and outbreaks

--- frontend/src/hooks/database/analyses/useGetAllAnalyses.ts
+++ frontend/src/hooks/database/analyses/useGetAllAnalyses.ts
@@ -4,3 +4,7 @@ import { useLiveQuery } from "dexie-react-hooks";
 export const useGetAllAnalyses = () => {
     return useLiveQuery(() => db.analyses.toArray());
 };
+
+export const useGetAnalysisByID = (id: number | undefined) => {
+    return useLiveQuery(() => db.analyses.get(id || -1), [id]);
+};
