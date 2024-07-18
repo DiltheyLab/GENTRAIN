cbf09606408974dfcffcfa6497877c09ea281e16
Philipp Vogel <philipp.vogel@nuuone.de>
2024-07-18 16:39:29 +0200

    feat: add error handling

--- frontend/src/services/errors.ts
+++ frontend/src/services/errors.ts
@@ -1,13 +1,28 @@
+import { toast } from "@/components/ui/use-toast";
 import { GentrainException } from "@/exceptions/GentrainException";
 import { t } from "i18next";
 import { ZodError } from "zod";
 
-export const getToastDescription = (error: GentrainException | ZodError | Error) => {
+export const getToastDescription = (error: GentrainException | ZodError | Error, category = "upload") => {
     let description = null;
     if (error instanceof GentrainException) {
-        description = t(`error:upload.${error.message}`, { data: error.data ? error.data.join(", ") : [] });
+        description = t(`error:${category}.${error.message}`, { data: error.data ? error.data.join(", ") : [] });
     } else if (error instanceof ZodError) {
-        description = t(`error:upload.${error.message}`);
+        description = t(`error:${category}.${error.message}`);
     }
     return description;
+};
+
+export const handleError = (error: any) => {
+    if (error instanceof GentrainException || error instanceof ZodError || error instanceof Error) {
+        toast({
+            title: t([`error:outbreakAnalysis.title`]),
+            description: getToastDescription(error, "outbreakAnalysis"),
+            duration: 10000,
+            variant: "destructive",
+        });
+        console.log(error, error.message);
+        return;
+    }
+    console.error("Error while saving analysis", error);
 };
