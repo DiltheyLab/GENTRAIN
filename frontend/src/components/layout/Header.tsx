dbec8dbc80a8aaa2ab8a88ffbf9d914043d8628d
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2024-07-19 17:35:15 +0200

    chore: test deployment

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -20,7 +20,7 @@ export const Header = () => {
             <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
                 <Link to="/" className="flex items-center gap-2 text-md font-semibold md:text-base">
                     <Share2 className="h-6 w-6 text-primary" />
-                    <span className="not-sr-only text-primary text-xl">Gentrain</span>
+                    <span className="not-sr-only text-primary text-xl">Hi Philipp</span>
                 </Link>
                 <Link to="/" className={`${isSelected("/")} transition-colors hover:text-foreground`}>
                     Dashboard
