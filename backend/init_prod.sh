52b6a3bfa1a43b56a07761d8dd0845a9297b09c6
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2024-07-20 06:50:25 +0200

    adjust entrypoint

--- backend/init_prod.sh
+++ backend/init_prod.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-chown -R uwsgi .
 chmod +x scripts/nextclade.sh
 chmod +x scripts/usher_nearest_k.sh
 chmod +x scripts/IMS_to_fasta.sh
 nextclade dataset get --name 'sars-cov-2' --output-dir '/app/datasets/nextclade_covid/'
-su uwsgi
 uwsgi --ini uwsgi.ini
