82d2e177afe849bd2e72f4cfdd523fd8e41add77
Philipp Vogel <philipp.vogel@nuuone.de>
2024-07-22 14:26:14 +0200

    feat: add switches to activate background data or data with low genetic relations

--- frontend/src/components/outbreakAnalysis/BackgroundSelection.tsx
+++ frontend/src/components/outbreakAnalysis/BackgroundSelection.tsx
@@ -69,6 +69,14 @@ export const BackgroundSelection = () => {
         analysisStore.updateSettings({ selectedBackground: selectedBackground });
     };
 
+    const handleIncludeCasesWithoutOutbreak = (value: boolean) => {
+        analysisStore.updateSettings({ includeCasesWithoutOutbreak: value });
+    };
+
+    const handleIncludeCasesWithLowGeneticDistance = (value: boolean) => {
+        analysisStore.updateSettings({ includeCasesWithLowGeneticDistance: value });
+    };
+
     return (
         <div className="flex flex-col gap-4">
             <Label className="font-bold text-lg">2. Background festlegen</Label>
@@ -84,16 +92,16 @@ export const BackgroundSelection = () => {
                 }
                 groupBy="group"
             />
-            {/*             <div className="flex flex-row items-center gap-3">
+            <div className="flex flex-row items-center gap-3">
                 <Switch
-                    id="ignoreBackground"
-                    checked={analysisStore.settings.ignoreBackground}
-                    onCheckedChange={(values) => handleIgnoreBackground(values)}
+                    id="includeCasesWithoutOutbreak"
+                    checked={analysisStore.settings.includeCasesWithoutOutbreak}
+                    onCheckedChange={(value) => handleIncludeCasesWithoutOutbreak(value)}
                 />
-                <Label htmlFor="ignoreBackground" className="font-normal text-base">
-                    Alle Daten als Background verwenden
+                <Label htmlFor="includeCasesWithoutOutbreak" className="font-normal text-base">
+                    Daten verwenden, die keinem Ausbruch zugewiesen sind
                 </Label>
-            </div> */}
+            </div>
             <div className="flex flex-row items-center gap-3">
                 <Switch
                     id="ignoreBackground"
@@ -102,6 +110,16 @@ export const BackgroundSelection = () => {
                 />
                 <Label htmlFor="ignoreBackground" className="font-normal text-base">
                     Background ausblenden
+                </Label>
+            </div>
+            <div className="flex flex-row items-center gap-3">
+                <Switch
+                    id="includeCasesWithLowGeneticDistance"
+                    checked={analysisStore.settings.ignoreBackground}
+                    onCheckedChange={(value) => handleIncludeCasesWithLowGeneticDistance(value)}
+                />
+                <Label htmlFor="includeCasesWithLowGeneticDistance" className="font-normal text-base">
+                    Daten verwenden die unter dem genetischen Distanzschwellenwert liegen
                 </Label>
             </div>
         </div>
