295dfd5da39860eb6b9340d418c64ef192548eda
PhilippVogel92 <philipp242@live.de>
2024-07-24 14:31:06 +0200

    feat: enable background options after selecting outbreak

--- frontend/src/components/outbreakAnalysis/AnalysisSettings.tsx
+++ frontend/src/components/outbreakAnalysis/AnalysisSettings.tsx
@@ -31,10 +31,10 @@ export const AnalysisSettings = () => {
     return (
         <div className="relative flex-col items-center gap-8 flex" x-chunk="dashboard-03-chunk-0">
             <form className="w-full items-start gap-3">
-                <fieldset className="flex flex-col gap-6 rounded-lg border p-4">
+                <fieldset className="flex flex-col gap-6 rounded-lg border p-4 min-h-[80vh]">
                     <div className="flex flex-col gap-3">
                         <OutbreakSelection />
-                        <BackgroundSelection />
+                        {analysisStore.settings.selectedOutbreak && <BackgroundSelection />}
                         <Button type="button" onClick={() => safeAnalysis()}>
                             Analyse speichern
                         </Button>
