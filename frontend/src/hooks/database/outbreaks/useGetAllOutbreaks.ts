87e823fa547ba3d716b64df2d0e37d6f51534f1c
PhilippVogel92 <philipp242@live.de>
2024-07-29 09:13:12 +0200

    feat: sort outbreak selection alphabetical order

--- frontend/src/hooks/database/outbreaks/useGetAllOutbreaks.ts
+++ frontend/src/hooks/database/outbreaks/useGetAllOutbreaks.ts
@@ -2,5 +2,7 @@ import { db } from "@/database/db";
 import { useLiveQuery } from "dexie-react-hooks";
 
 export const useGetAllOutbreaks = () => {
-    return useLiveQuery(() => db.outbreaks.toArray());
+    return useLiveQuery(() =>
+        db.outbreaks.toArray().then((outbreaks) => outbreaks.sort((a, b) => a.name.localeCompare(b.name)))
+    );
 };
