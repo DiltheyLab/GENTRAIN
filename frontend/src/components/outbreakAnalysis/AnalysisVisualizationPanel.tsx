b0087b371142057e840a82372c6b0014de863676
PhilippVogel92 <philipp242@live.de>
2024-07-29 13:04:29 +0200

    refactor: move legend to own component

--- frontend/src/components/outbreakAnalysis/AnalysisVisualizationPanel.tsx
+++ frontend/src/components/outbreakAnalysis/AnalysisVisualizationPanel.tsx
@@ -11,6 +11,7 @@ import { AnalysisGraph } from "./AnalysisGraph";
 import { CaseWithRelationships } from "@/database/cases";
 import { DistanceMatrixAssembly } from "@/database/distance_matrices";
 import { AnalysisGraphSettings } from "./AnalysisGraphSettings";
+import { Legend } from "./Legend";
 
 export const AnalysisVisualizationPanel = () => {
     const containerRef = useRef<HTMLDivElement>(null);
@@ -52,29 +53,11 @@ export const AnalysisVisualizationPanel = () => {
         }
     };
 
-    const getLegend = () => {
-        const nodes = analyseStore.graphData.nodes;
-        const uniqueGroups = nodes
-            .filter((group, index, self) => {
-                return index === self.findIndex((node) => node.group === group.group);
-            })
-            .sort((a, b) => a.group.localeCompare(b.group));
-        return uniqueGroups.map((node) => (
-            <div className="flex items-center gap-2" key={node.group}>
-                <span style={{ backgroundColor: `${node.color}` }} className={"rounded-full h-3 w-3"} />
-                <p>{node.group}</p>
-            </div>
-        ));
-    };
-
     return (
         <div ref={containerRef} className="relative flex h-full flex-col rounded-xl bg-muted lg:col-span-2">
             {analyseStore.settings.selectedOutbreak && (
                 <>
-                    <fieldset className="absolute z-10 left-2 top-2 rounded-lg w-fit border p-4 bg-muted">
-                        <legend className="-ml-1 px-1 text-sm font-medium">Legende</legend>
-                        <div className="flex flex-col">{getLegend()}</div>
-                    </fieldset>
+                    <Legend />
                     <AnalysisGraphSettings
                         showGraphSettings={showGraphSettings}
                         updateShowGraphSettings={(showGraphSettings) => setShowGraphSettings(showGraphSettings)}
