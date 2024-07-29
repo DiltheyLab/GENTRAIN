39540af04a7b763d799cbdc5689b0b9e5e1374e9
PhilippVogel92 <philipp242@live.de>
2024-07-29 15:58:19 +0200

    fix: delete RKI path

--- backend/init.sh
+++ backend/init.sh
@@ -4,5 +4,4 @@ chmod +x scripts/nextclade.sh
 chmod +x scripts/usher_nearest_k.sh
 chmod +x scripts/IMS_to_fasta.sh
 nextclade dataset get --name 'sars-cov-2' --output-dir '/app/datasets/nextclade_covid/'
-cd datasets/RKI
 flask run --host=0.0.0.0 -p 4000
