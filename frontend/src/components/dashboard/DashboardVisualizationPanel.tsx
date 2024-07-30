b10b98d304bac33ed7c8271952a60218cdac3355
PhilippVogel92 <philipp242@live.de>
2024-07-30 11:19:34 +0200

    refactor: add legend component and spinner to dashboard graph

--- frontend/src/components/dashboard/DashboardVisualizationPanel.tsx
+++ frontend/src/components/dashboard/DashboardVisualizationPanel.tsx
@@ -4,13 +4,14 @@ import { ForcedDirectedGraph2D } from "../graphs/ForcedDirectedGraph";
 import { ForcedDirectedGraph3D } from "../graphs/ForcedDirectedGraph3D";
 import { useEffect, useMemo, useRef } from "react";
 import { deepCopyData } from "@/lib/utils";
-import { getUniqueSamplingTimes, transformDistanceMatrixToGraphData } from "@/services/graphs";
-import { Label } from "@/components/ui/label";
+import { transformDistanceMatrixToGraphData } from "@/services/graphs";
 import { useAppStore } from "@/stores/app";
 import { useResizeContainer } from "@/hooks/useResizeContainer";
 import { type GraphData, useGraphStore } from "@/stores/graph";
 import { useGetDistanceMatrixAssemblyByPathogenId } from "@/hooks/database/distance_matrices/useGetDistanceMatrixAssemblyByPathogenId";
 import { useGetAllCasesWithRelationships } from "@/hooks/database/cases/useGetAllCasesWithRelationships";
+import { Legend } from "../outbreakAnalysis/Legend";
+import { Loader2 } from "lucide-react";
 
 export const DashboardVisualizationPanel = () => {
     const containerRef = useRef<HTMLDivElement>(null);
@@ -47,30 +48,6 @@ export const DashboardVisualizationPanel = () => {
         }
     };
 
-    const getLegend = () => {
-        const nodes = graphStore.data.nodes;
-        if (graphStore.settings.coloring === "normal" || graphStore.settings.coloring === "outbreaks") {
-            const uniqueGroups = nodes.filter((group, index, self) => {
-                return index === self.findIndex((t) => t.group === group.group);
-            });
-            return uniqueGroups.map((node) => (
-                <div className="flex items-center gap-2" key={node.group}>
-                    <span style={{ backgroundColor: `${node.color}` }} className={"rounded-full h-3 w-3"} />
-                    <p>{node.group}</p>
-                </div>
-            ));
-        } else if (graphStore.settings.coloring === "registered_at") {
-            const uniqueSamplingTimes = getUniqueSamplingTimes(nodes);
-
-            return uniqueSamplingTimes.map((node) => (
-                <div className="flex items-center gap-2" key={node.registeredAt}>
-                    <span style={{ backgroundColor: `${node.color}` }} className={"rounded-full h-3 w-3"} />
-                    <p>{node.registeredAt || "Kein Datum angegeben"}</p>
-                </div>
-            ));
-        }
-    };
-
     return (
         <div ref={containerRef} className="relative flex h-full flex-col rounded-xl bg-muted lg:col-span-2">
             <Badge variant="outline" className="absolute z-50 right-3 top-3">
@@ -79,17 +56,10 @@ export const DashboardVisualizationPanel = () => {
             <Button variant="outline" className="absolute z-50 bottom-3 right-3">
                 Reset
             </Button>
-            <fieldset className="absolute z-10 left-2 top-2 rounded-lg w-fit border p-4 bg-muted">
-                <legend className="-ml-1 px-1 text-sm font-medium">Legende</legend>
-                <div className="flex flex-col">
-                    <Label htmlFor="role" className="mb-2">
-                        Ausbrüche
-                    </Label>
-                    {getLegend()}
-                </div>
-            </fieldset>
+            <Legend nodes={graphStore.data.nodes} />
+
             <div className=" flex justify-center items-center h-full w-full" id="graph-container">
-                {getGraph()}
+                {graphStore.data.nodes.length !== 0 ? getGraph() : <Loader2 className="h-24 w-h-24 animate-spin" />}
             </div>
         </div>
     );
