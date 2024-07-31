24c3085936ab72e8eafb0c64c46d1aa9016544be
PhilippVogel92 <philipp242@live.de>
2024-07-31 16:36:19 +0200

    refactor: delete width of date range picker

--- frontend/src/components/outbreakAnalysis/DateRangePicker.tsx
+++ frontend/src/components/outbreakAnalysis/DateRangePicker.tsx
@@ -35,10 +35,7 @@ export const DateRangePicker = ({ className }: React.HTMLAttributes<HTMLDivEleme
                         id="date"
                         disabled={!analysisStore.settings.excludeCasesOutsideOfDateRange}
                         variant="outline"
-                        className={cn(
-                            "w-[300px] justify-start text-left font-normal",
-                            !date && "text-muted-foreground"
-                        )}
+                        className={cn("justify-start text-left font-normal", !date && "text-muted-foreground")}
                     >
                         <CalendarIcon className="mr-2 h-4 w-4" />
                         {date?.from ? (
