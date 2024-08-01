fdbf4e7b594dc364a8a86b19d4524026b13211ee
PhilippVogel92 <philipp242@live.de>
2024-08-01 08:18:24 +0200

    chore: improve comments

--- frontend/src/hooks/database/groups/useGetAllGroups.ts
+++ frontend/src/hooks/database/groups/useGetAllGroups.ts
@@ -9,9 +9,9 @@ export type GroupWithCategory = GroupSchema & { categoryName: string | undefined
 
 export const useGetAllGroupsAndOutbreaks = () => {
     return useLiveQuery(async () => {
-        //get groups with categories
+        //get groups, sort them alphabetically and add the category name
         const groups = (await db.groups.toArray()).sort((a, b) => a.name.localeCompare(b.name));
-        const groupsWithCategories: GroupWithCategory[] = await Promise.all(
+        const groupsWithCategories = await Promise.all(
             groups.map(async (group) => {
                 const category = await db.categories.get(group.category_id);
                 const categoryName = category?.name;
@@ -19,11 +19,11 @@ export const useGetAllGroupsAndOutbreaks = () => {
             })
         );
 
-        //get outbreaks
+        //get outbreaks and sort them alphabetically
         const outbreaks = (await db.outbreaks.toArray()).sort((a, b) => a.name.localeCompare(b.name));
 
-        //check for cases without outbreak
         const cases = await db.cases.toArray();
+        //check for cases without outbreak (not possible in indexedDB because null is not a indexable type)
         const casesWithoutOutbreakExist = cases.some((c) => c.outbreak_id === null);
 
         return { outbreaks, groupsWithCategories, casesWithoutOutbreakExist };
