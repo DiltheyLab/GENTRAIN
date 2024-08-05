941dd6af79742e7a5c03cc336890760d909fb6b6
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2024-08-05 19:27:30 +0200

    add deploy folder

--- backend/scripts/pathogens/covid-19.sh
+++ backend/scripts/pathogens/covid-19.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # run nextclade
-nextclade run $1 --output-json $2 --input-dataset datasets/nextclade_covid
+nextclade run $1 --output-json $2 --input-dataset /home/backend/datasets/nextclade_covid
