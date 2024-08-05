ba3bf22ce15f326f0d1700286a082e7bb1d96e6c
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2024-08-05 18:13:40 +0200

    add deploy folder

--- backend/backend/exceptions/sequence_analysis_failed_exception.py
+++
@@ -1,5 +0,0 @@
-class SequenceAnalysisFailedException(Exception):
-    """Exception thrown whenever a sequence analysis fails."""
-
-    def __init__(self):
-        self.message = "Genomic sequence analysis failed."
--- backend/backend/init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-nextclade dataset get --name 'sars-cov-2' --output-dir '/backend/datasets/nextclade_covid/'
-flask run --host=0.0.0.0 -p 4000
