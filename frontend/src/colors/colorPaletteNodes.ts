64e028304dbe3ea2eb9f8b820ce0d072047f1860
PhilippVogel92 <philipp242@live.de>
2024-08-05 13:24:26 +0200

    feat: add color palette for nodes

---
+++ frontend/src/colors/colorPaletteNodes.ts
@@ -0,0 +1,88 @@
+export const COLORPALETTENODES = [
+    "#FF0000", // red for selected outbreak
+    "#FFD700", // gold
+    "#8A2BE2", // blue violet
+    "#FF7F50", // coral
+    "#00FA9A", // medium spring green
+    "#B22222", // firebrick
+    "#ADFF2F", // green yellow
+    "#00FF00", // green
+    "#0000FF", // blue
+    "#FFFF00", // yellow
+    "#FF00FF", // magenta
+    "#00FFFF", // cyan
+    "#800000", // maroon
+    "#808000", // olive
+    "#008000", // dark green
+    "#800080", // purple
+    "#008080", // teal
+    "#000080", // navy
+    "#FF4500", // orange red
+    "#2E8B57", // sea green
+    "#DAA520", // goldenrod
+    "#4B0082", // indigo
+    "#FF1493", // deep pink
+    "#7FFF00", // chartreuse
+    "#D2691E", // chocolate
+    "#5F9EA0", // cadet blue
+    "#FF6347", // tomato
+    "#4682B4", // steel blue
+    "#9ACD32", // yellow green
+    "#FF69B4", // hot pink
+    "#CD5C5C", // indian red
+    "#6A5ACD", // slate blue
+    "#32CD32", // lime green
+    "#CCCCCC", // last color
+];
+
+export const COLORPALETTEGRADATIONS = [
+    "#FF0000", // red for selected outbreak
+    "#FF3300",
+    "#FF6600",
+    "#FF9933",
+    "#FFCC33",
+    "#FFFF33",
+    "#CCFF33",
+    "#99FF33",
+    "#66FF33",
+    "#33FF33",
+    "#33FF66",
+    "#33FF99",
+    "#33FFCC",
+    "#33FFFF",
+    "#33CCFF",
+    "#3399FF",
+    "#3366FF",
+    "#3333FF",
+    "#6633FF",
+    "#9933FF",
+    "#CC33FF",
+    "#FF33FF",
+    "#FF33CC",
+    "#FF3399",
+    "#FF3366",
+    "#FF3333",
+    "#FF6633",
+    "#FF9966",
+    "#FFCC99",
+    "#FFFFCC",
+    "#CCFFFF",
+    "#99FFFF",
+    "#66FFFF",
+    "#33FFFF",
+    "#33CCFF",
+    "#3399FF",
+    "#3366FF",
+    "#3333FF",
+    "#6633FF",
+    "#9933FF",
+    "#CC33FF",
+    "#FF33FF",
+    "#FF33CC",
+    "#FF3399",
+    "#FF3366",
+    "#FF3333",
+    "#FF6666",
+    "#FF9999",
+    "#CCCCCC", // last color
+];
