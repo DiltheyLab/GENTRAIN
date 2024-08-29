e36fb3d2e836fc50c1fba334f7a1d6e1a337f984
PhilippVogel92 <philipp242@live.de>
2024-08-29 12:23:21 +0200

    feat: add session to core store

--- frontend/src/modules/core/stores/core.ts
+++ frontend/src/modules/core/stores/core.ts
@@ -1,22 +1,36 @@
-import { db } from "@/modules/core/infrastructure/database";
+import { db, SessionsSchema } from "@/modules/core/infrastructure/database";
 import { create } from "zustand";
 import { PathogenSchema, PathogenWithRelationships } from "@/modules/core/models/pathogens";
 
 interface CoreState {
     activePathogen: PathogenSchema | null;
+    session: SessionsSchema | undefined | null; //SessionSchema
+    fetchSession: () => void;
+    initSession: () => void;
     updateActivePathogen: (pathogen: PathogenSchema) => void;
 }
 
-export const useCoreStore = create<CoreState>((set, get) => ({
-    activePathogen: null,
-    updateActivePathogen: (pathogen: PathogenWithRelationships) => {
-        const activePathogen = get().activePathogen;
-        if (activePathogen && activePathogen?.id !== pathogen.id) {
-            db.pathogens.update(activePathogen.id, { activated_at: null });
-        }
-        if (activePathogen?.id !== pathogen.id) {
-            db.pathogens.update(pathogen.id, { activated_at: new Date().toISOString() });
-        }
-        set({ activePathogen: pathogen });
-    },
-}));
+export const useCoreStore = create<CoreState>((set, get) => {
+    return {
+        activePathogen: null,
+        session: undefined,
+        fetchSession: async () => {
+            const session = (await db.sessions.toCollection().first()) ?? null;
+            set({ session });
+        },
+        initSession: async () => {
+            const sessionId = await db.sessions.add({});
+            set({ session: { id: sessionId } });
+        },
+        updateActivePathogen: async (pathogen: PathogenWithRelationships) => {
+            const activePathogen = get().activePathogen;
+            if (activePathogen && activePathogen?.id !== pathogen.id) {
+                db.pathogens.update(activePathogen.id, { activated_at: null });
+            }
+            if (activePathogen?.id !== pathogen.id) {
+                db.pathogens.update(pathogen.id, { activated_at: new Date().toISOString() });
+            }
+            set({ activePathogen: pathogen });
+        },
+    };
+});
