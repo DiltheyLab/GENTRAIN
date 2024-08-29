46439a608262a6d6f9d872df76abef958d43a8c4
PhilippVogel92 <philipp242@live.de>
2024-08-29 09:32:00 +0200

    fix: use reactive state for active pathogen

--- frontend/src/modules/core/hooks/database/analyses/useGetAnalysesForActivePathogen.ts
+++ frontend/src/modules/core/hooks/database/analyses/useGetAnalysesForActivePathogen.ts
@@ -3,7 +3,7 @@ import { useCoreStore } from "@/modules/core/stores/core";
 import { useLiveQuery } from "dexie-react-hooks";
 
 export const useGetAnalysesForActivePathogen = (): AnalysisSchema[] | undefined => {
-    const activePathogen = useCoreStore.getState().activePathogen;
+    const activePathogen = useCoreStore((state) => state.activePathogen);
     return useLiveQuery(() => {
         if (!activePathogen) {
             return;
