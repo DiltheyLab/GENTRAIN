a6f6ab7dacb54a556ebd94424c7fd1ac0d2dcade
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2024-08-30 10:34:07 +0200

    add cors

--- backend/server.py
+++ backend/server.py
@@ -31,11 +31,13 @@ if os.environ.get("FLASK_ENV") == "development":
         ],
     )
 else:
-    CORS(app)
+
+    CORS(app, resources={r"/*": {"origins": "*"}})
     socketio = SocketIO(
         app,
         message_queue="redis://redis:6379",
         max_http_buffer_size=MAX_BUFFER_SIZE,
+        cors_allowed_origins="https://gentrain.bi.denbi.de",
     )
 
 app.register_blueprint(api, url_prefix="/api")
