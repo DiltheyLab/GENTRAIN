79f3ac89425d56bac75223b1479d8c5c5aad69fb
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2024-08-30 16:59:02 +0200

    chore: start multiple workers

--- backend/worker.py
+++ backend/worker.py
@@ -1,11 +1,11 @@
 import redis
+import sys
 from rq import Worker, Queue, Connection
 
-listen = ["viral", "bacterial"]
-
-conn = redis.from_url("redis://gentrain-redis:6379")
+redis_conn = redis.from_url("redis://gentrain-redis:6379")
 
 if __name__ == "__main__":
-    with Connection(conn):
-        worker = Worker(list(map(Queue, listen)))
+    queue_name = sys.argv[1] if len(sys.argv) > 1 else "default"
+    with Connection(redis_conn):
+        worker = Worker([Queue(queue_name)])
         worker.work()
