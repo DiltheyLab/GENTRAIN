04c450b8702b998311361805025487e009db0001
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2024-08-30 19:24:49 +0200

    chore: change supervisor directory

--- frontend/src/modules/core/helpers/socket.ts
+++ frontend/src/modules/core/helpers/socket.ts
@@ -3,6 +3,7 @@ import { io } from "socket.io-client";
 export const socket =
     import.meta.env.VITE_ENABLE_WEBSOCKETS === "true" &&
     io(import.meta.env.VITE_API_HOST, {
+        transports: ["websocket"],
         extraHeaders: {
             Authorization:
                 "Basic " + btoa(`${import.meta.env.VITE_HTBASIC_USERNAME}:${import.meta.env.VITE_HTBASIC_PASSWORD}`),
