4dc4d2614e5ca8d50e5ca810eb9a6208f77bfbc6
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2024-08-30 12:52:28 +0200

    chore: disbale long polling

--- frontend/src/modules/core/helpers/socket.ts
+++ frontend/src/modules/core/helpers/socket.ts
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
 export const socket = io(import.meta.env.VITE_API_HOST, {
+    transports: ["websocket"],
     extraHeaders: {
         Authorization:
             "Basic " + btoa(`${import.meta.env.VITE_HTBASIC_USERNAME}:${import.meta.env.VITE_HTBASIC_PASSWORD}`),
