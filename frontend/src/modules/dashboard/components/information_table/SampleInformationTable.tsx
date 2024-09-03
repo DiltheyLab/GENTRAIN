def8714d4d27b41f2a5ec9ff2b2c1933ea58b63a
PhilippVogel92 <philipp242@live.de>
2024-09-03 13:45:43 +0200

    refactor: adapt new information to sampleInformationTable

--- frontend/src/modules/dashboard/components/information_table/SampleInformationTable.tsx
+++ frontend/src/modules/dashboard/components/information_table/SampleInformationTable.tsx
@@ -10,9 +10,10 @@ export function SampleInformationTable() {
             <TableRow className="font-medium bg-muted">
                 <TableHead className="font-medium p-2 text-xs text-black">Fall ID</TableHead>
                 <TableHead className="font-medium p-2 text-xs text-black">Sequenz ID</TableHead>
-                <TableHead className="font-medium p-2 text-xs text-black">Lineage</TableHead>
-                <TableHead className="font-medium p-2 text-xs text-black">Ambigious Characters</TableHead>
-                <TableHead className="font-medium p-2 text-xs text-black">Letztes Änderungsdatum</TableHead>
+                <TableHead className="font-medium p-2 text-xs text-black">N's</TableHead>
+                <TableHead className="font-medium p-2 text-xs text-black">Ausbruch</TableHead>
+                <TableHead className="font-medium p-2 text-xs text-black">Registrierungsdatum</TableHead>
+                <TableHead className="font-medium p-2 text-xs text-black">Gruppen</TableHead>
             </TableRow>
         );
     };
@@ -23,10 +24,15 @@ export function SampleInformationTable() {
                 <TableRow key={caseData.id} className="border-muted">
                     <TableCell className="p-2 text-xs font-medium">{caseData.case_id}</TableCell>
                     <TableCell className="p-2 text-xs font-medium">{caseData.sample?.fasta_id}</TableCell>
-                    <TableCell className="p-2 text-xs font-medium">{caseData.sample?.lineage}</TableCell>
                     <TableCell className="p-2 text-xs font-medium">{caseData.sample?.n_count}</TableCell>
-                    <TableCell className="p-2 text-xs">
-                        {caseData.sample?.updated_at ? caseData.sample?.updated_at.toLocaleDateString() : ""}
+                    <TableCell className="p-2 text-xs font-medium">
+                        {caseData.outbreak?.name ?? "Keinem Ausbruch zugewiesen"}
+                    </TableCell>
+                    <TableCell className="p-2 text-xs font-medium">
+                        {caseData.registered_at.toLocaleDateString()}
+                    </TableCell>
+                    <TableCell className="p-2 text-xs font-medium">
+                        {caseData.groups?.map((group) => group.name).join(", ") ?? "Keiner Gruppe zugewiesen"}
                     </TableCell>
                 </TableRow>
             );
