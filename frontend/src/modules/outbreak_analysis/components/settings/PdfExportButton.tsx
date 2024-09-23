6e18206c6d673e26519f0adae2513e8be5a0bb9d
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2024-09-23 10:34:44 +0200

    chore: adjust legent and introduction

--- frontend/src/modules/outbreak_analysis/components/settings/PdfExportButton.tsx
+++ frontend/src/modules/outbreak_analysis/components/settings/PdfExportButton.tsx
@@ -6,19 +6,24 @@ import { Document, Page, View, StyleSheet, Image } from "@react-pdf/renderer";
 import "@/assets/css/pdf.css";
 import { useOutbreakAnalysisStore } from "../../stores/outbreakAnalysis";
 import { ColorMap } from "@/modules/core/types/graph";
-import { getSelectedClusters } from "@/modules/core/helpers/graphs";
+import { getSelectedClusters, getUniqueTypesOfLinks } from "@/modules/core/helpers/graphs";
 import MerriweatherRegular from "@/assets/font/Merriweather_Sans/MerriweatherSans-Regular.ttf";
 import MerriweatherItalic from "@/assets/font/Merriweather_Sans/MerriweatherSans-Italic.ttf";
 import MerriweatherLight from "@/assets/font/Merriweather_Sans/MerriweatherSans-Light.ttf";
 import MerriweatherLightItalic from "@/assets/font/Merriweather_Sans/MerriweatherSans-LightItalic.ttf";
 import MerriweatherSemiBold from "@/assets/font/Merriweather_Sans/MerriweatherSans-SemiBold.ttf";
 import MerriweatherBold from "@/assets/font/Merriweather_Sans/MerriweatherSans-Bold.ttf";
+<<<<<<< HEAD
 import { Graph2D } from "@/modules/core/components/graph/Graph2D";
 import { useEffect, useRef, useState } from "react";
 import { useResizeContainer } from "@/modules/core/hooks/useResizeContainer";
+=======
+import { useEffect, useMemo, useState } from "react";
+>>>>>>> 6388381 (chore: adjust legent and introduction)
 import { useCoreStore } from "@/modules/core/stores/core";
 import { GraphPdf } from "@/modules/core/components/graph/GraphPdf";
 import { PathogenTypeName } from "@/modules/core/models/pathogen_types";
+import { t } from "i18next";
 
 Font.register({
     family: "Merriweather",
@@ -79,10 +84,20 @@ const AnalysisReport = ({ graphImage }: { graphImage: string }) => {
     const selectedBackgroundNames = selectedClusters.selectedBackground;
     const activePathogen = coreState.activePathogen;
     const nodes = outbreakAnalysisState.graphData.nodes;
+    const links = outbreakAnalysisState.graphData.links;
+    const uniqueTypesOfLinks = useMemo(() => getUniqueTypesOfLinks(links), [links]);
+    const geneticDistanceLinks = uniqueTypesOfLinks.filter((link) => link.type === t(`linkTypes.geneticDistance`));
+    const uniqueContactTracingLinks = uniqueTypesOfLinks.filter((link) => link.type !== t(`linkTypes.geneticDistance`));
+    const allContactTracingLinks = links.filter((link) => link.type !== t(`linkTypes.geneticDistance`));
+
+    const caseCountWithoutOutbreak = nodes.filter((node) => !node.caseData.outbreak).length;
+    const caseCountInSelectedOutbreak = nodes.filter(
+        (node) => node.caseData.outbreak?.name === selectedOutbreakName
+    ).length;
     const getTable = () => {
         return (
             <>
-                <Headline level={2}>Analysierte Sequenzdaten</Headline>
+                <Headline level={2}>Analysierte Falldaten</Headline>
                 <View style={{ fontSize: 8 }}>
                     <View
                         style={{
@@ -152,6 +167,12 @@ const AnalysisReport = ({ graphImage }: { graphImage: string }) => {
                             )}
                         </View>
                     ))}
+                    <Text style={{ fontSize: 8, fontStyle: "italic", paddingTop: 10, borderTop: "1px solid #0F172A" }}>
+                        Tabelle 1: Falldaten nach Fall-Nummer aus Abbildung 1 inklusive relevanter Parameter zur
+                        Beurteilung der Qualität der{" "}
+                        {activePathogen?.pathogen_type?.name === PathogenTypeName.viral ? "viralen" : "bakteriellen"}{" "}
+                        Sequenzen.
+                    </Text>
                 </View>
             </>
         );
@@ -161,17 +182,19 @@ const AnalysisReport = ({ graphImage }: { graphImage: string }) => {
         return (
             <View
                 style={{
-                    flexDirection: "row",
+                    flexDirection: "column",
                     justifyContent: "center",
                     flexWrap: "wrap",
                 }}
             >
-                <View style={{ flexDirection: "row", margin: 5 }}>
+                <Text style={{ fontSize: 6, fontWeight: 600, marginTop: 5 }}>Untersuchter Ausbruch</Text>
+                <View style={{ flexDirection: "row", marginTop: 5 }}>
                     <div
                         style={{
+                            marginTop: 1,
                             marginRight: 5,
-                            width: 10,
-                            height: 10,
+                            width: 5,
+                            height: 5,
                             borderRadius: "50%",
                             backgroundColor: colorMap[outbreakName].color,
                         }}
@@ -179,50 +202,92 @@ const AnalysisReport = ({ graphImage }: { graphImage: string }) => {
                     <Text
                         style={{
                             color: "#000000",
-                            fontSize: 10,
+                            fontSize: 6,
                         }}
                     >
                         {outbreakName}
                     </Text>
                 </View>
-                {backgroundNames.map((name: string, key: number) => (
-                    <View key={key} style={{ flexDirection: "row", margin: 5 }}>
-                        <div
-                            style={{
-                                marginRight: 5,
-                                width: 10,
-                                height: 10,
-                                borderRadius: "50%",
-                                backgroundColor: colorMap[name].color,
-                            }}
-                        ></div>
-                        <Text
-                            style={{
-                                color: "#000000",
-                                fontSize: 10,
-                            }}
-                        >
-                            {name}
-                        </Text>
-                    </View>
-                ))}
+                {backgroundNames.length > 0 && (
+                    <Text style={{ fontSize: 6, fontWeight: 600, marginTop: 5 }}>Weitere Fälle</Text>
+                )}
+                {backgroundNames.map((name: string, key: number) => {
+                    return (
+                        <View key={key} style={{ flexDirection: "row", marginTop: 5 }}>
+                            <div
+                                style={{
+                                    marginTop: 1,
+                                    marginRight: 5,
+                                    width: 5,
+                                    height: 5,
+                                    borderRadius: "50%",
+                                    backgroundColor: colorMap[name].color,
+                                }}
+                            ></div>
+                            <Text
+                                style={{
+                                    color: "#000000",
+                                    fontSize: 6,
+                                }}
+                            >
+                                {name}
+                            </Text>
+                        </View>
+                    );
+                })}
+                {geneticDistanceLinks.length > 0 && (
+                    <Text style={{ fontSize: 6, fontWeight: 600, marginTop: 5 }}>Genetische Kanten</Text>
+                )}
+                {geneticDistanceLinks.map((link, key) => {
+                    return (
+                        <View key={key} style={{ flexDirection: "row", marginTop: 5 }}>
+                            <div
+                                style={{
+                                    backgroundColor: `${link.color}`,
+                                    height: 1,
+                                    width: 5,
+                                    marginTop: 3,
+                                    marginRight: 5,
+                                }}
+                            ></div>
+                            <Text style={{ fontSize: 6 }}>{link.type}</Text>
+                        </View>
+                    );
+                })}
+                {uniqueContactTracingLinks.length > 0 && (
+                    <Text style={{ fontSize: 6, fontWeight: 600, marginTop: 5 }}>Kontaktkanten</Text>
+                )}
+                {uniqueContactTracingLinks.map((link, key) => {
+                    return (
+                        <View key={key} style={{ flexDirection: "row", marginTop: 5 }}>
+                            <div
+                                style={{
+                                    backgroundColor: `${link.color}`,
+                                    height: 1,
+                                    width: 5,
+                                    marginTop: 3,
+                                    marginRight: 5,
+                                }}
+                            ></div>
+                            <Text style={{ fontSize: 6 }}>{link.type}</Text>
+                        </View>
+                    );
+                })}
             </View>
         );
     };
 
     const getSummary = () => {
-        const selectedOutbreakName = selectedClusters.selectedOutbreak[0];
-        const selectedBackgroundNames = selectedClusters.selectedBackground;
-        const caseCountWithoutOutbreak = nodes.filter((node) => !node.caseData.outbreak).length;
-        const caseCountInSelectedOutbreak = nodes.filter(
-            (node) => node.caseData.outbreak?.name === selectedOutbreakName
-        ).length;
         return (
             <View>
                 <Paragraph>
-                    Der analysierte Datensatz umfasst {caseCountInSelectedOutbreak}{" "}
-                    {caseCountInSelectedOutbreak > 1 ? "Fälle" : "Fall"} des zu untersuchenden vermuteten Ausbruchs "
-                    {selectedOutbreakName}"
+                    Der analysierte Datensatz umfasst Falldaten des{" "}
+                    {activePathogen?.pathogen_type?.name === PathogenTypeName.viral ? "viralen" : "bakteriellen"}{" "}
+                    Pathogens "{activePathogen?.name}".
+                </Paragraph>
+                <Paragraph>
+                    Es existieren {caseCountInSelectedOutbreak} {caseCountInSelectedOutbreak > 1 ? "Fälle" : "Fall"} des
+                    zu untersuchenden vermuteten Ausbruchs "{selectedOutbreakName}"
                     {selectedBackgroundNames.map((clusterName: string, index: number) => {
                         const caseCountInCluster = nodes.filter(
                             (node) => node.caseData.outbreak?.name === clusterName
@@ -244,11 +309,17 @@ const AnalysisReport = ({ graphImage }: { graphImage: string }) => {
                         <Text>
                             {" "}
                             sowie {caseCountWithoutOutbreak} {caseCountWithoutOutbreak > 1 ? "Fälle" : "Fall"} aus der
-                            Umgebung ohne eine Ausbruchszuweisung.
+                            Umgebung ohne Ausbruchszuweisung.
                         </Text>
                     ) : (
                         <Text>.</Text>
                     )}
+                </Paragraph>
+                <Paragraph>
+                    Für {nodes.length - caseCountWithoutOutbreak} von {nodes.length} Fällen liegen genetische
+                    Sequenzdaten vor.
+                    {allContactTracingLinks.length > 0 &&
+                        ` Es sind außerdem ${allContactTracingLinks.length} Kontaktangaben enthalten.`}
                 </Paragraph>
             </View>
         );
@@ -288,13 +359,27 @@ const AnalysisReport = ({ graphImage }: { graphImage: string }) => {
                     <View
                         style={{
                             width: "100%",
-                            alignItems: "center",
+                            flexDirection: "row",
+                            alignItems: "flex-start",
                             justifyContent: "center",
+                            marginTop: 10,
                         }}
                     >
                         <Image source={graphImage} style={{ marginBottom: 15 }} />
                         {getLegend(colorMap, selectedOutbreakName, selectedBackgroundNames)}
                     </View>
+                    <Text style={{ fontSize: 8, fontStyle: "italic", marginTop: 10 }}>
+                        Abbildung 1: Minimum Spanning Tree (MST) der analysierten Fälle. Jeder Knoten im MST
+                        repräsentiert einen gemeldeten Fall; Knoten-Farben zeigen den Falltyp an (Umgebungsproben oder
+                        als potentieller Ausbruch gekennzeichnete Proben). Genetische Abstände zwischen den
+                        sequenzierten{" "}
+                        {activePathogen?.pathogen_type?.name === PathogenTypeName.viral ? "viralen" : "bakteriellen"}{" "}
+                        Genomen werden über graue Kanten zwischen Punkten visualisiert, die mit dem jeweiligen
+                        genetischen Abstand beschriftet sind.
+                        {allContactTracingLinks.length > 0 &&
+                            " Kontakte zwischen Fällen sind durch farbliche Kanten repräsentiert."}{" "}
+                        Zahlen in den Knoten beziehen sich auf die Spalte "Fall-Nummer im MST" in Tabelle 1.
+                    </Text>
                 </View>
             </Page>
         </Document>
@@ -352,7 +437,7 @@ const PdfExportButton = () => {
                             linkDistance={50}
                             nodeSize={10}
                             showNodeLabel={showNodeLabel}
-                            linkWidth={1}
+                            linkWidth={2}
                             updateSelectedCase={() => {}}
                             selectedCase={null}
                             exportPdfOnEngineStop={() => setGraphReadyForExport(true)}
