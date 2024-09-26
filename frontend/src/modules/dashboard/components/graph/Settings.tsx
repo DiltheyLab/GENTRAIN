4e62488c6c618f7c4f4e8caeea9545b62fe57226
PhilippVogel92 <philipp242@live.de>
2024-09-26 10:34:54 +0200

    feat: add responsive div sizes for dashboard and outbreakAnalysis

--- frontend/src/modules/dashboard/components/graph/Settings.tsx
+++ frontend/src/modules/dashboard/components/graph/Settings.tsx
@@ -2,20 +2,18 @@ import { ColorSelection } from "./ColorSelection";
 
 export const DashboardSettings = () => {
     return (
-        <div className="flex-col items-center flex">
-            <form className="w-full">
-                <fieldset className="flex flex-col gap-4 rounded-lg border p-4">
-                    <div>
-                        <div className="flex flex-col">
-                            <h1 className="text-xl font-semibold leading-none">Einfärbung</h1>
-                            <p className="text-sm text-muted-foreground mb-2 mt-1">
-                                Färben Sie den Graphen nach verschiedenen Kriterien ein.
-                            </p>
-                        </div>
-                        <ColorSelection />
+        <form className="flex flex-col items-center w-full">
+            <fieldset className="flex flex-col gap-4 rounded-lg border p-4">
+                <div>
+                    <div className="flex flex-col">
+                        <h1 className="text-xl font-semibold leading-none">Einfärbung</h1>
+                        <p className="text-sm text-muted-foreground mb-2 mt-1">
+                            Färben Sie den Graphen nach verschiedenen Kriterien ein.
+                        </p>
                     </div>
-                </fieldset>
-            </form>
-        </div>
+                    <ColorSelection />
+                </div>
+            </fieldset>
+        </form>
     );
 };
