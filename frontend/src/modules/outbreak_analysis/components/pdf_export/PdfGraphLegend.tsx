c0e7ab94790a4612077a3f9495f7c8885e42a239
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2024-09-27 09:34:58 +0200

    refactor: cleanup legend component

--- frontend/src/modules/outbreak_analysis/components/pdf_export/PdfGraphLegend.tsx
+++ frontend/src/modules/outbreak_analysis/components/pdf_export/PdfGraphLegend.tsx
@@ -16,127 +16,46 @@ const PdfGraphLegend = ({ preview = false }: { preview?: boolean }) => {
     const geneticDistanceLinks = uniqueTypesOfLinks.filter((link) => link.type === t(`linkTypes.geneticDistance`));
     const uniqueContactTracingLinks = uniqueTypesOfLinks.filter((link) => link.type !== t(`linkTypes.geneticDistance`));
 
-    if (preview) {
+    const SectionHeadline = ({ children }: { children: string }) => {
         return (
-            <div className="flex flex-col justify-center">
-                <p className="text-[10px] font-bold mt-[5px]">Untersuchter Ausbruch</p>
-                <div className="flex mt-[5px] items-center">
-                    <div
-                        style={{
-                            marginRight: 5,
-                            width: 5,
-                            height: 5,
-                            borderRadius: "50%",
-                            backgroundColor: colorMap[selectedOutbreakName].color,
-                        }}
-                    ></div>
-                    <p className="text-[10px]">{selectedOutbreakName}</p>
-                </div>
-                {selectedBackgroundNames.length > 0 && <p className="text-[10px] font-bold mt-[5px]">Weitere Fälle</p>}
-                {selectedBackgroundNames.map((name: string, key: number) => {
-                    return (
-                        <div key={key} className="flex items-center mt-[5px]">
-                            <div
-                                style={{
-                                    marginRight: 5,
-                                    width: 5,
-                                    height: 5,
-                                    borderRadius: "50%",
-                                    backgroundColor: colorMap[name].color,
-                                }}
-                            ></div>
-                            <p
-                                style={{
-                                    color: "#000000",
-                                    fontSize: 10,
-                                    textAlign: "left",
-                                }}
-                            >
-                                {name}
-                            </p>
-                        </div>
-                    );
-                })}
-                {geneticDistanceLinks.length > 0 && <p className="text-[10px] font-bold mt-[5px]">Genetische Kanten</p>}
-                {geneticDistanceLinks.map((link, key) => {
-                    return (
-                        <div key={key} className="flex mt-[5px] items-center">
-                            <div
-                                style={{
-                                    backgroundColor: `${link.color}`,
-                                    height: 1,
-                                    width: 5,
-                                    marginRight: 5,
-                                }}
-                            ></div>
-                            <p className="text-[10px]">{link.type}</p>
-                        </div>
-                    );
-                })}
-                {uniqueContactTracingLinks.length > 0 && (
-                    <p style={{ fontSize: 10, fontWeight: 600, marginTop: 5 }}>Kontaktkanten</p>
+            <>
+                {preview ? (
+                    <p className="font-bold mt-[5px]">{children}</p>
+                ) : (
+                    <Text style={{ fontSize: 6, fontWeight: 600, marginTop: 5 }}>{children}</Text>
                 )}
-                {uniqueContactTracingLinks.map((link, key) => {
-                    return (
-                        <div key={key} className="flex mt-[5px] items-center">
-                            <div
-                                style={{
-                                    backgroundColor: `${link.color}`,
-                                    height: 1,
-                                    width: 5,
-                                    marginRight: 5,
-                                }}
-                            ></div>
-                            <p style={{ fontSize: 10 }}>{link.type}</p>
-                        </div>
-                    );
-                })}
-            </div>
+            </>
         );
-    }
-    return (
-        <View
-            style={{
-                flexDirection: "column",
-                justifyContent: "center",
-            }}
-        >
-            <Text style={{ fontSize: 6, fontWeight: 600, marginTop: 5 }}>Untersuchter Ausbruch</Text>
-            <View style={{ flexDirection: "row", marginTop: 5 }}>
-                <div
-                    style={{
-                        marginTop: 1,
-                        marginRight: 5,
-                        width: 5,
-                        height: 5,
-                        borderRadius: "50%",
-                        backgroundColor: colorMap[selectedOutbreakName].color,
-                    }}
-                ></div>
-                <Text
-                    style={{
-                        color: "#000000",
-                        fontSize: 6,
-                        textAlign: "left",
-                    }}
-                >
-                    {selectedOutbreakName}
-                </Text>
-            </View>
-            {selectedBackgroundNames.length > 0 && (
-                <Text style={{ fontSize: 6, fontWeight: 600, marginTop: 5 }}>Weitere Fälle</Text>
-            )}
-            {selectedBackgroundNames.map((name: string, key: number) => {
-                return (
-                    <View key={key} style={{ flexDirection: "row", marginTop: 5 }}>
+    };
+
+    const Entry = ({ type, color, children }: { type: string; color: string; children: string }) => {
+        return (
+            <>
+                {preview ? (
+                    <div className="flex mt-[5px] items-center">
+                        <div
+                            className={`${
+                                type === "node"
+                                    ? "rounded-full w-[5px] h-[5px] ml-[2px] mr-[7px]"
+                                    : "w-[9px] h-[2px] mr-[5px]"
+                            }`}
+                            style={{
+                                backgroundColor: color,
+                            }}
+                        ></div>
+                        <p>{children}</p>
+                    </div>
+                ) : (
+                    <View style={{ flexDirection: "row", marginTop: 5 }}>
                         <div
                             style={{
-                                marginTop: 1,
-                                marginRight: 5,
-                                width: 5,
-                                height: 5,
-                                borderRadius: "50%",
-                                backgroundColor: colorMap[name].color,
+                                marginTop: type === "node" ? 1 : 3,
+                                marginLeft: type === "node" ? 2 : 0,
+                                marginRight: type === "node" ? 7 : 5,
+                                width: type === "node" ? 5 : 9,
+                                height: type === "node" ? 5 : 1,
+                                borderRadius: type === "node" ? "50%" : 0,
+                                backgroundColor: color,
                             }}
                         ></div>
                         <Text
@@ -146,50 +65,60 @@ const PdfGraphLegend = ({ preview = false }: { preview?: boolean }) => {
                                 textAlign: "left",
                             }}
                         >
-                            {name}
+                            {children}
                         </Text>
                     </View>
-                );
-            })}
-            {geneticDistanceLinks.length > 0 && (
-                <Text style={{ fontSize: 6, fontWeight: 600, marginTop: 5 }}>Genetische Kanten</Text>
+                )}
+            </>
+        );
+    };
+
+    const getLabels = () => {
+        return (
+            <>
+                <SectionHeadline>Untersuchter Ausbruch</SectionHeadline>
+                <Entry type="node" color={colorMap[selectedOutbreakName].color}>
+                    {selectedOutbreakName}
+                </Entry>
+                <SectionHeadline>Weitere Fälle</SectionHeadline>
+                {selectedBackgroundNames.map((name: string, key: number) => (
+                    <Entry key={key} type="node" color={colorMap[name].color}>
+                        {name}
+                    </Entry>
+                ))}
+                <SectionHeadline>Genetische Kanten</SectionHeadline>
+                {geneticDistanceLinks.map((link, key) => (
+                    <Entry key={key} type="link" color={link.color}>
+                        {link.type}
+                    </Entry>
+                ))}
+                <SectionHeadline>Kontaktkanten</SectionHeadline>
+                {uniqueContactTracingLinks.map((link, key) => (
+                    <>
+                        <Entry key={key} type="link" color={link.color}>
+                            {link.type}
+                        </Entry>
+                    </>
+                ))}
+            </>
+        );
+    };
+
+    return (
+        <>
+            {preview ? (
+                <div className="flex flex-col justify-center text-[10px]">{getLabels()}</div>
+            ) : (
+                <View
+                    style={{
+                        flexDirection: "column",
+                        justifyContent: "center",
+                    }}
+                >
+                    {getLabels()}
+                </View>
             )}
-            {geneticDistanceLinks.map((link, key) => {
-                return (
-                    <View key={key} style={{ flexDirection: "row", marginTop: 5 }}>
-                        <div
-                            style={{
-                                backgroundColor: `${link.color}`,
-                                height: 1,
-                                width: 5,
-                                marginTop: 3,
-                                marginRight: 5,
-                            }}
-                        ></div>
-                        <Text style={{ fontSize: 6, textAlign: "left" }}>{link.type}</Text>
-                    </View>
-                );
-            })}
-            {uniqueContactTracingLinks.length > 0 && (
-                <Text style={{ fontSize: 6, fontWeight: 600, marginTop: 5 }}>Kontaktkanten</Text>
-            )}
-            {uniqueContactTracingLinks.map((link, key) => {
-                return (
-                    <View key={key} style={{ flexDirection: "row", marginTop: 5 }}>
-                        <div
-                            style={{
-                                backgroundColor: `${link.color}`,
-                                height: 1,
-                                width: 5,
-                                marginTop: 3,
-                                marginRight: 5,
-                            }}
-                        ></div>
-                        <Text style={{ fontSize: 6 }}>{link.type}</Text>
-                    </View>
-                );
-            })}
-        </View>
+        </>
     );
 };
 
