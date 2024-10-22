62bd2c3c608f50e1c90ac847e78f469a572a61eb
benjoka <ben.kraeling@study.hs-duesseldorf.de>
2024-10-22 13:57:11 +0200

    fix: html translation renderer keys

--- frontend/src/modules/core/helpers/translations.tsx
+++ frontend/src/modules/core/helpers/translations.tsx
@@ -5,19 +5,19 @@ export const renderHtmlFromTranslation = (i18nKey: string) => {
     const elements: { tag: "p" | "code"; content: string }[] | { tag: "ul"; content: string[] }[] = t(i18nKey, {
         returnObjects: true,
     });
-    return elements.map((element) => {
+    return elements.map((element, elementIndex) => {
         switch (element.tag) {
             case "p":
                 return (
-                    <p>
+                    <p key={`${element.tag}_${elementIndex}`}>
                         <Trans shouldUnescape>{element.content}</Trans>
                     </p>
                 );
             case "ul":
                 return (
-                    <ul className="list-disc pl-4">
-                        {element.content.map((child) => (
-                            <li>
+                    <ul className="list-disc pl-4" key={`${element.tag}_${elementIndex}`}>
+                        {element.content.map((child, listIndex) => (
+                            <li key={`${element.tag}_${elementIndex}_li_${listIndex}`}>
                                 <Trans shouldEscape>{child}</Trans>
                             </li>
                         ))}
@@ -25,7 +25,7 @@ export const renderHtmlFromTranslation = (i18nKey: string) => {
                 );
             case "code":
                 return (
-                    <code className="p-8 bg-muted rounded-lg">
+                    <code className="p-8 bg-muted rounded-lg" key={`${element.tag}_${elementIndex}`}>
                         <Trans shouldUnescape>{element.content}</Trans>
                     </code>
                 );
