df6ab7a5053ef43b7d0bfb3077b8526bc76309f2
benjoka <ben.kraeling@icloud.com>
2024-11-18 13:43:21 +0100

    chore: adjust vite config env retrieval

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    envDir: process.env.NODE_ENV === 'development' ? "../" : ".env",
+    envDir: process.env.NODE_ENV === 'development' ? "../" : "./",
     server: {
         port: 3000,
     },
