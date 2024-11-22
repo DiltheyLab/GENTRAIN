17571f135bdf872f7220902b57c3edfed0d5a6a5
PhilippVogel92 <philipp242@live.de>
2024-11-22 11:32:32 +0100

    feat: add posthog

--- frontend/src/modules/core/main.tsx
+++ frontend/src/modules/core/main.tsx
@@ -1,44 +1,49 @@
 import React from "react";
 import * as ReactDOM from "react-dom/client";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import {Dashboard} from "@/modules/dashboard/pages/Dashboard.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Dashboard } from "@/modules/dashboard/pages/Dashboard.tsx";
 import "@/modules/core/index.css";
-import {Error} from "@/modules/core/pages/Error.tsx";
-import {DataManagement} from "@/modules/data_management/pages/DataManagement.tsx";
-import {I18nextProvider} from "react-i18next";
+import { Error } from "@/modules/core/pages/Error.tsx";
+import { DataManagement } from "@/modules/data_management/pages/DataManagement.tsx";
+import { I18nextProvider } from "react-i18next";
 import i18next from "i18next";
 import translation_de from "@/modules/core/translations/de/common.json";
 import import_de from "@/modules/core/translations/de/import.json";
 import error_de from "@/modules/core/translations/de/error.json";
-import {Toaster} from "@/modules/core/components/ui/Toaster.tsx";
-import {OutbreakAnalysisOverview} from "@/modules/outbreak_analysis/pages/OutbreakAnalysisOverview";
-import {OutbreakAnalysis} from "@/modules/outbreak_analysis/pages/OutbreakAnalysis.tsx";
-import {Root} from "./Root";
-import {Impress} from "./pages/Impress";
-import {Contact} from "./pages/Contact";
+import { Toaster } from "@/modules/core/components/ui/Toaster.tsx";
+import { OutbreakAnalysisOverview } from "@/modules/outbreak_analysis/pages/OutbreakAnalysisOverview";
+import { OutbreakAnalysis } from "@/modules/outbreak_analysis/pages/OutbreakAnalysis.tsx";
+import { Root } from "./Root";
+import { Impress } from "./pages/Impress";
+import { Contact } from "./pages/Contact";
 import { DataPrivacy } from "./pages/DataPrivacy";
+import { PostHogProvider } from "posthog-js/react";
+
+const options = {
+    api_host: import.meta.env.VITE_APP_PUBLIC_POSTHOG_HOST,
+};
 
 i18next.init({
-    interpolation: {escapeValue: false},
+    interpolation: { escapeValue: false },
     lng: "de",
     resources: {
-        de: {translation: translation_de, import: import_de, error: error_de},
+        de: { translation: translation_de, import: import_de, error: error_de },
     },
 });
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Root/>,
-        errorElement: <Error/>,
+        element: <Root />,
+        errorElement: <Error />,
         children: [
-            {path: "/", element: <Dashboard/>},
-            {path: "/impress", element: <Impress/>},
-            {path: "/data-privacy", element: <DataPrivacy/>},
-            {path: "/contact", element: <Contact/>},
-            {path: "/outbreak-analysis", element: <OutbreakAnalysisOverview/>},
-            {path: "/outbreak-analysis/:name", element: <OutbreakAnalysis/>},
-            {path: "/data-management", element: <DataManagement/>},
+            { path: "/", element: <Dashboard /> },
+            { path: "/impress", element: <Impress /> },
+            { path: "/data-privacy", element: <DataPrivacy /> },
+            { path: "/contact", element: <Contact /> },
+            { path: "/outbreak-analysis", element: <OutbreakAnalysisOverview /> },
+            { path: "/outbreak-analysis/:name", element: <OutbreakAnalysis /> },
+            { path: "/data-management", element: <DataManagement /> },
         ],
     },
 ]);
@@ -46,8 +51,9 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <I18nextProvider i18n={i18next}>
-            <Toaster/>
-            <RouterProvider router={router}/>
+            <Toaster />
+            <PostHogProvider apiKey={import.meta.env.VITE_APP_PUBLIC_POSTHOG_KEY} options={options}></PostHogProvider>
+            <RouterProvider router={router} />
         </I18nextProvider>
     </React.StrictMode>
 );
