b1ca9b3a282bd639d3a277756c96564baab7cd63
benjoka <ben.kraeling@icloud.com>
2024-11-25 14:17:37 +0100

    feat: secure redis

--- backend/modules/sequence_analysis/strategies/sequence_analysis_strategy.py
+++ backend/modules/sequence_analysis/strategies/sequence_analysis_strategy.py
@@ -7,8 +7,9 @@ from redis import Redis
 from flask_socketio import SocketIO
 from backend.modules.core.exceptions import GenomicErrorException, SequenceAnalysisFailedException
 
-redis_connection = Redis(host=environ.get('REDIS_HOST'), port=environ.get('REDIS_PORT'), decode_responses=True)
-sio = SocketIO(message_queue=f"redis://{environ.get('REDIS_HOST')}:{environ.get('REDIS_PORT')}")
+redis_connection = Redis(host=environ.get('REDIS_HOST'), port=environ.get('REDIS_PORT'), username=environ.get('REDIS_USERNAME'),
+                         password=environ.get('REDIS_PASSWORD'), decode_responses=True)
+sio = SocketIO(message_queue=f"redis://{environ.get('REDIS_USERNAME')}:{environ.get('REDIS_PASSWORD')}@{environ.get('REDIS_HOST')}:{environ.get('REDIS_PORT')}")
 
 
 class SequenceAnalysisStrategy(ABC):
--- backend/worker_viral.py
+++ backend/worker_viral.py
@@ -3,7 +3,8 @@ from os import environ
 import redis
 from rq import Worker, Queue, Connection
 
-redis_conn = redis.from_url(f"redis://{environ.get('REDIS_HOST')}:{environ.get('REDIS_PORT')}")
+print("credentials", environ.get('REDIS_USERNAME'), environ.get('REDIS_PASSWORD'), f"redis://{environ.get('REDIS_USERNAME')}:{environ.get('REDIS_PASSWORD')}@{environ.get('REDIS_HOST')}:{environ.get('REDIS_PORT')}")
+redis_conn = redis.from_url(f"redis://{environ.get('REDIS_USERNAME')}:{environ.get('REDIS_PASSWORD')}@{environ.get('REDIS_HOST')}:{environ.get('REDIS_PORT')}")
 
 if __name__ == "__main__":
     with Connection(redis_conn):
