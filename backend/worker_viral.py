f6d2ea41518c048059df1c8d6bdec899a9ce4060
benjoka <ben.kraeling@icloud.com>
2024-11-25 14:43:24 +0100

    cleanup: remove print

--- backend/worker_viral.py
+++ backend/worker_viral.py
@@ -3,7 +3,6 @@ from os import environ
 import redis
 from rq import Worker, Queue, Connection
 
-print("credentials", environ.get('REDIS_USERNAME'), environ.get('REDIS_PASSWORD'), f"redis://{environ.get('REDIS_USERNAME')}:{environ.get('REDIS_PASSWORD')}@{environ.get('REDIS_HOST')}:{environ.get('REDIS_PORT')}")
 redis_conn = redis.from_url(f"redis://{environ.get('REDIS_USERNAME')}:{environ.get('REDIS_PASSWORD')}@{environ.get('REDIS_HOST')}:{environ.get('REDIS_PORT')}")
 
 if __name__ == "__main__":
