6116262528fdb0b131c53b8cdb41ed28615783a8
benjoka <ben.kraeling@icloud.com>
2024-12-02 13:18:16 +0100

    fix: CORS for dev

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
+from os import environ
 from flask_cors import CORS
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_basicauth import BasicAuth
 
 app = Flask(__name__)
-CORS(app)
+if environ.get("APP_ENV") == "development":
+    CORS(app)
 app.config.from_pyfile("config.py")
 basic_auth = BasicAuth(app)
 db = SQLAlchemy(app)
