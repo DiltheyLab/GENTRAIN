c0a2b6050c0ea68c203d55d37379db658f841f2c
benjoka <ben.kraeling@icloud.com>
2024-12-20 14:02:22 +0100

    fix: docs search

--- backend/modules/docs/routes.py
+++ backend/modules/docs/routes.py
@@ -1,8 +1,11 @@
-from flask import  send_from_directory
+from flask import send_from_directory
 from backend.app import app
+
 
 @app.route('/docs/')
 @app.route('/docs/<path:path>')
 def mkdocs(path=""):
     """Serve the index.html file."""
-    return send_from_directory(f"{app.static_folder}/docs", f"{path}index.html" if "assets" not in path else f"{path}")
+    return send_from_directory(f"{app.static_folder}/docs",
+                               f"{path}index.html" if "assets" not in path and "search" not in path else
+                               f"{path}")
