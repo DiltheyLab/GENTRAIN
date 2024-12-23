e0df586ce7519e472d776079cf6aa761e856205e
benjoka <ben.kraeling@icloud.com>
2024-12-23 12:18:30 +0100

    feat: validate .genes_list

--- backend/modules/admin/strategies/bacterial_scheme_validator.py
+++ backend/modules/admin/strategies/bacterial_scheme_validator.py
@@ -1,5 +1,8 @@
-from backend.modules.admin.strategies.scheme_validator_strategy import SchemeValidatorStrategy
+import re
 
+from wtforms.validators import ValidationError
+
+from backend.modules.admin.strategies.scheme_validator_strategy import SchemeValidatorStrategy
 
 class BacterialSchemeValidator(SchemeValidatorStrategy):
     """Concrete validator strategy for viral scheme validator."""
@@ -10,7 +13,23 @@ class BacterialSchemeValidator(SchemeValidatorStrategy):
     def validate_zip(self):
         """Concrete template method for viral zip validation."""
         self.validate_filenames()
+        self.validate_genes_list()
+        #self.validate_schema_config()
         self.validate_fasta_files()
+
+    def validate_genes_list(self):
+        decoded = self.zip_file.read(".genes_list").decode('utf-8', errors='ignore')
+        filename_pattern = re.compile(r'\b[\w\-.]+\.fasta\b')
+        fasta_filenames = filename_pattern.findall(decoded)
+        zip_filenames = self.zip_file.namelist()
+        for filename in fasta_filenames:
+            if filename not in zip_filenames:
+                raise ValidationError(f"Gene file {filename} is missing.")
+
+    def validate_schema_config(self):
+        # WIP: how to validate this file?
+        decoded = self.zip_file.read(".schema_config")
+
 
     def fill_clean_zip(self, filename, zip_out):
         zip_out.writestr(".genes_list", self.zip_file.read(".genes_list"))
