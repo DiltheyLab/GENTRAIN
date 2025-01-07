c5a55934fda49f646e181b74efa28e6c5e79409f
benjoka <ben.kraeling@icloud.com>
2025-01-07 11:49:26 +0100

    fix: remove migration automation

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,13 @@
-import os
-import subprocess
 from os import environ
 
 from flask_cors import CORS
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_basicauth import BasicAuth
+
 app = Flask(__name__)
 if environ.get("APP_ENV") == "development":
     CORS(app)
 app.config.from_pyfile("config.py")
 basic_auth = BasicAuth(app)
 db = SQLAlchemy(app)
-os.system("alembic upgrade head")
