4dd7b0a2663d23d9fdd6727186cc0e70cab100ab
PhilippVogel92 <philipp242@live.de>
2025-01-08 17:56:52 +0100

    fix: delete unused import

--- frontend/src/modules/core/components/graph/Graph2D.tsx
+++ frontend/src/modules/core/components/graph/Graph2D.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import ForceGraph2D, { ForceGraphMethods, LinkObject, NodeObject } from "react-force-graph-2d";
 import { Loader2 } from "lucide-react";
