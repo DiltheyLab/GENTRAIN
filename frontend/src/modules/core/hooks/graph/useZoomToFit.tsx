ceae0fee90b36d921efad2909fe90d2703542246
PhilippVogel92 <philipp242@live.de>
2025-01-08 17:52:26 +0100

    feat: implement custom hook to trigger manual zoom

--- frontend/src/modules/core/hooks/graph/useZoomToFit.tsx
+++ frontend/src/modules/core/hooks/graph/useZoomToFit.tsx
@@ -1,9 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-export const useZoomToFit = (zoomToFitTriggers: Array<any>, updateZoomToFit: () => void) => {
+export const useZoomToFit = (initialZoomToFit: boolean, zoomToFitTriggers: Array<any>) => {
+    const [zoomToFit, setZoomToFit] = useState(initialZoomToFit);
+
     //listens to triggers which can activate a zoom out
     useEffect(() => {
         if (zoomToFitTriggers.length === 0) return;
-        updateZoomToFit();
+        setZoomToFit(true);
     }, zoomToFitTriggers);
+
+    return [zoomToFit, setZoomToFit] as const;
 };
