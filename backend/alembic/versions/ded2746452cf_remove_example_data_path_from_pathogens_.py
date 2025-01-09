e8a4457c1704fb2b01cd74d523ce7ffa5514ee3c
benjoka <ben.kraeling@icloud.com>
2025-01-09 11:11:05 +0100

    chore: remove migration

---
+++ backend/alembic/versions/ded2746452cf_remove_example_data_path_from_pathogens_.py
@@ -0,0 +1,25 @@
+"""remove example data path from pathogens table
+
+Revision ID: ded2746452cf
+Revises: adb98933166a
+Create Date: 2025-01-09 10:10:32.939030
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'ded2746452cf'
+down_revision: Union[str, None] = 'adb98933166a'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.drop_column('pathogen', 'example_data_path')
+
+def downgrade() -> None:
+    op.add_column('pathogen', sa.Column('example_data_path', sa.String(), nullable=True))
