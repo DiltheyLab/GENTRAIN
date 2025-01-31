9d97dd5228e9781925dac5bd1dda5a7eed78673a
benjoka <ben.kraeling@icloud.com>
2025-01-31 15:50:55 +0100

    refactor: restructure contacts persistence strategy

--- frontend/src/modules/data_management/services/data_import/persistence/ContactsPersistence.ts
+++ frontend/src/modules/data_management/services/data_import/persistence/ContactsPersistence.ts
@@ -1,6 +1,7 @@
 import { toast } from "@/modules/core/components/ui/UseToast";
 import { db } from "@/modules/core/services/database/DatabaseManager";
 import {
+    ContactImport,
     ContactSchema,
     contactRules,
     createContactsFromAddresses,
@@ -14,7 +15,6 @@ import { t } from "i18next";
 
 export class ContactsPersistence extends PersistenceStrategy {
     protected persist = async () => {
-        const bulkData = [] as ContactSchema[];
         const contactImports = useDataManagementStore.getState().contactImports;
         // create Set of caseIds to fetch them from the database
         const caseIds = new Set<string>();
@@ -27,27 +27,9 @@ export class ContactsPersistence extends PersistenceStrategy {
         const cases = await db.cases.where("case_id").anyOf(Array.from(caseIds)).toArray();
         const casesMap = ObjectRelationalMapper.arrayToMap(cases, "case_id");
 
-        for (const contactId of Object.keys(contactImports)) {
-            const contact = contactImports[contactId];
-
-            const case1 = casesMap.get(contact.imported.case_id_1);
-            const case2 = casesMap.get(contact.imported.case_id_2);
-
-            if (!case1 || !case2 || !contact.import) {
-                continue;
-            }
-
-            // Validate the data and throw an error if it is invalid
-            const dto = contactRules.parse({
-                case_id_1: case1.id,
-                case_id_2: case2.id,
-                type: contact.imported.type,
-                context: contact.imported.context,
-            }) as ContactSchema;
-            bulkData.push(dto);
-        }
         // Bulk add the data to the database
-        await db.contacts.bulkAdd(bulkData);
+        await db.contacts.bulkAdd(this.collectContactImports(contactImports, casesMap));
+
         useDataManagementStore.getState().setContactSelectionActive(false);
         useDataManagementStore.getState().clearContactImports();
         if (!useDataManagementStore.getState().showImportAssistent) {
@@ -108,5 +90,36 @@ export class ContactsPersistence extends PersistenceStrategy {
         createContactsFromAddresses(addressMap, ObjectRelationalMapper.arrayToMap(cases) as Map<number, CaseSchema>);
     }
 
-    protected update = async () => {};
+    private collectContactImports(
+        contactImports: {
+            [id: string]: {
+                imported: ContactImport;
+                persisted: ContactSchema | null;
+                import: boolean;
+            };
+        },
+        casesMap: Map<string, CaseSchema>
+    ) {
+        const bulkData = [] as ContactSchema[];
+        for (const contactId of Object.keys(contactImports)) {
+            const contact = contactImports[contactId];
+
+            const case1 = casesMap.get(contact.imported.case_id_1);
+            const case2 = casesMap.get(contact.imported.case_id_2);
+
+            if (!case1 || !case2 || !contact.import) {
+                continue;
+            }
+
+            // Validate the data and throw an error if it is invalid
+            const dto = contactRules.parse({
+                case_id_1: case1.id,
+                case_id_2: case2.id,
+                type: contact.imported.type,
+                context: contact.imported.context,
+            }) as ContactSchema;
+            bulkData.push(dto);
+        }
+        return bulkData;
+    }
 }
