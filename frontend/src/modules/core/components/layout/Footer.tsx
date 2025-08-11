85e97156e4f4aafd57e5f21200133fc9b0fa7c69
PhilippVogel92 <philipp242@live.de>
2025-08-11 10:43:11 +0200

    refactor: remove unused session ID from Footer component

--- frontend/src/modules/core/components/layout/Footer.tsx
+++ frontend/src/modules/core/components/layout/Footer.tsx
@@ -1,9 +1,6 @@
 import { PartnerLogos } from "@/modules/core/components/layout/PartnerLogos";
-import { useCoreStore } from "../../stores/core";
 
 export const Footer = () => {
-    const sessionId = useCoreStore((state) => state.sessionId);
-
     return (
         <footer className="mt-[40px] bg-accent">
             <div className="flex justify-between items-center px-4 py-4 max-w-[1500px] mx-auto text-sm min-h-[80px]">
@@ -25,7 +22,6 @@ export const Footer = () => {
                         <a href="/impress" className="underline ml-4">
                             Impressum
                         </a>
-                        <p className="mt-3"> Session ID: {sessionId}</p>
                     </div>
                 </div>
                 <div className="flex items-center justify-evenly relative">
