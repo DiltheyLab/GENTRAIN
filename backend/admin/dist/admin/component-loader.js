765eaca7865f58bb04214093de02cca95139784a
PhilippVogel92 <philipp242@live.de>
2025-08-19 05:21:29 +0200

    refactor: delete dist folder from github

--- backend/admin/dist/admin/component-loader.js
+++
@@ -1,9 +0,0 @@
-import { ComponentLoader } from 'adminjs';
-import path from 'path';
-import * as url from 'url';
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-export const componentLoader = new ComponentLoader();
-export const add = (componentName, url) => componentLoader.add(componentName, path.join(__dirname, url));
-export const override = (componentName, url) => componentLoader.override(componentName, path.join(__dirname, url));
-override('Login', 'components/Login');
-export const DASHBOARD = add('Dashboard', './components/Dashboard');
--- backend/admin/dist/admin/components/Dashboard.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Box, H1, Text } from '@adminjs/design-system';
-const Dashboard = () => {
-    return (React.createElement(Box, { variant: "grey" },
-        React.createElement(Box, { variant: "white", p: "xl", textAlign: "center" },
-            React.createElement(H1, { fontWeight: "lighter" }, "GENTRAIN Admin"),
-            React.createElement(Text, { fontWeight: "lighter", mt: "default" }, "Willkommen im GENTRAIN Admin-Bereich! Hier k\u00F6nnen Sie die Pathogen-Datenbank verwalten, Benutzer f\u00FCr das Admin-Panel anlegen und entsprechende Benutzetrollen zuweisen. In der linken Seitenleiste finden Sie die Navigation zu den verschiedenen Bereichen."))));
-};
-export default Dashboard;
