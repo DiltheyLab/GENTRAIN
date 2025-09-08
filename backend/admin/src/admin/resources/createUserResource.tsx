be037ef0163c4007c3084a60159e7adbe70062d7
PhilippVogel92 <philipp242@live.de>
2025-09-08 11:47:22 +0200

    feat: implement password hashing and update timestamps in user resource actions

--- backend/admin/src/admin/resources/createUserResource.tsx
+++ backend/admin/src/admin/resources/createUserResource.tsx
@@ -1,8 +1,9 @@
-import { ResourceOptions } from 'adminjs';
+import { ListActionResponse, RecordActionResponse, ResourceOptions } from 'adminjs';
 import { navigation } from '../options.js';
 import { getModelByName } from '@adminjs/prisma';
 import { Prisma, PrismaClient } from '@prisma/client';
 import { DefaultArgs } from '@prisma/client/runtime/library';
+import { hash } from 'argon2';
 
 export const createUserResource = (prisma: PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>) => {
   return {
@@ -10,7 +11,8 @@ export const createUserResource = (prisma: PrismaClient<Prisma.PrismaClientOptio
     options: {
       navigation: navigation, // Add resource to navigation
       properties: {
-        email: {
+        username: {
+          position: 2,
           isRequired: true,
           type: 'string',
         },
@@ -18,6 +20,78 @@ export const createUserResource = (prisma: PrismaClient<Prisma.PrismaClientOptio
           isRequired: true,
           type: 'password',
           isVisible: { list: false, filter: false, show: false, edit: true },
+        },
+        confirmed_at: {
+          isVisible: {
+            list: true,
+            filter: true,
+            show: true,
+            edit: false,
+          },
+        },
+        created_at: {
+          isVisible: {
+            list: true,
+            filter: true,
+            show: true,
+            edit: false,
+          },
+        },
+        updated_at: {
+          isVisible: {
+            list: true,
+            filter: true,
+            show: true,
+            edit: false,
+          },
+        },
+      },
+      actions: {
+        new: {
+          before: async (request) => {
+            // hash password before saving
+            if (request.payload?.password) {
+              request.payload.password = await hash(request.payload.password);
+            }
+            return request;
+          },
+        },
+        show: {
+          after: async (response: RecordActionResponse) => {
+            // prevent password from being sent to the client
+            response.record.params.password = '';
+            return response;
+          },
+        },
+        edit: {
+          before: async (request) => {
+            // no need to hash on GET requests, we'll remove passwords there anyway
+            if (request.method === 'post') {
+              // hash only if password is present, delete otherwise
+              // so we don't overwrite it
+              if (request.payload?.password) {
+                request.payload.password = await hash(request.payload.password);
+                request.payload.updated_at = new Date();
+              } else {
+                delete request.payload?.password;
+              }
+            }
+            return request;
+          },
+          after: async (response: RecordActionResponse) => {
+            // prevent password from being sent to the client
+            response.record.params.password = '';
+            return response;
+          },
+        },
+        list: {
+          after: async (response: ListActionResponse) => {
+            // prevent password from being sent to the client
+            response.records.forEach((record) => {
+              record.params.password = '';
+            });
+            return response;
+          },
         },
       },
     } as ResourceOptions,
