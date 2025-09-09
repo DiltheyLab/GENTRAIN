794f4d1f7c6eb5e6a1e90b763c39b4a00f110d1e
PhilippVogel92 <philipp242@live.de>
2025-09-09 12:35:28 +0200

    feat: add db seed for superadmin

--- backend/admin/src/admin/constants.ts
+++ backend/admin/src/admin/constants.ts
@@ -1,10 +1,1 @@
-export type AuthUser = {
-  username: string;
-  password: string;
-};
-export const DEFAULT_ADMIN: AuthUser = {
-  username: process.env.DEFAULT_ADMIN || 'admin',
-  password: process.env.DEFAULT_ADMIN_PASSWORD || 'secretPassword',
-};
-
 export const SUPERUSER_ROLE = 'superuser';
