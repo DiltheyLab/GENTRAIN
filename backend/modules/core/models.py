dbc595d87d49096b9f6c9c39d46ea9e09ec14037
benjoka <ben.kraeling@icloud.com>
2025-09-24 10:05:58 +0200

    fix: add activated field to json response of pathogens

--- backend/modules/core/models.py
+++ backend/modules/core/models.py
@@ -48,6 +48,7 @@ class Pathogen(db.Model):
             "name": self.name,
             "genetic_distance_threshold": self.genetic_distance_threshold,
             "type": self.type,
+            activated: self.activated,
         }
 
     def get_example_data_path(self, file_type):
