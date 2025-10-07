9940af2d4f03e9cbb6f39a4c64e2daf47734a88a
benjoka <ben.kraeling@icloud.com>
2025-10-07 14:06:38 +0200

    test entrypoint

--- backend/api/entrypoint-api-prod.sh
+++ backend/api/entrypoint-api-prod.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+chown gentrain:gentrain /api/prisma
+chmod 755 /api/prisma
 prisma generate --generator py_client
 gunicorn --chdir / --workers 1 --threads 100 --bind :8080 api.server:app
