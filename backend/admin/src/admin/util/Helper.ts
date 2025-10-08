06f88cdc38f89753a952c4a134596417ec3cd80a
benjoka <ben.kraeling@icloud.com>
2025-10-08 14:53:33 +0200

    fix: BigInt representation in adminJs

--- backend/admin/src/admin/util/Helper.ts
+++ backend/admin/src/admin/util/Helper.ts
@@ -4,7 +4,7 @@ const getReadableSize = (schemeSizeInBytes: number) => {
   }
   const units = ['B', 'KB', 'MB', 'GB', 'TB'];
   let index = 0;
-  let readableSchemeSize = schemeSizeInBytes;
+  let readableSchemeSize = Number(schemeSizeInBytes);
 
   while (readableSchemeSize >= 1024 && index < units.length - 1) {
     readableSchemeSize /= 1024;
