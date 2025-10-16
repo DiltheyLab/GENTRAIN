b70d25c6de2e013eda0572698ae3aabbc1b3e317
benjoka <ben.kraeling@icloud.com>
2025-10-16 11:39:09 +0200

    fix: use correct gentrain user id

--- init-entrypoint.sh
+++ init-entrypoint.sh
@@ -4,7 +4,7 @@
 chown -R 65534:65534 /data /config /certs
 # Create gentrain user and gentrain group and set gentrain:gentrain as owner for the api_data-volume
 # To grant permission when writing to the volume from inside the admin container
-addgroup gentrain
-adduser -D -G gentrain gentrain
+addgroup -g 1010 gentrain
+adduser -D -G gentrain -u 1010 gentrain
 chown gentrain:gentrain -R /api_data
 exit 0
