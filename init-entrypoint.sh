4a3d68ed49f2ee738bdea5eac0d16f8b873bc9f0
benjoka <ben.kraeling@icloud.com>
2025-10-16 11:29:44 +0200

    remove group id

--- init-entrypoint.sh
+++ init-entrypoint.sh
@@ -4,7 +4,7 @@
 chown -R 65534:65534 /data /config /certs
 # Create gentrain user and gentrain group and set gentrain:gentrain as owner for the api_data-volume
 # To grant permission when writing to the volume from inside the admin container
-addgroup -g 1001 gentrain
-adduser -D -G gentrain -u 1001 gentrain
+addgroup gentrain
+adduser -D -G gentrain gentrain
 chown gentrain:gentrain -R /api_data
 exit 0
