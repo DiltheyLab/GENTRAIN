d6fb344afdda15aab841a73b6f2e9aab9584bb0e
benjoka <ben.kraeling@icloud.com>
2025-10-16 10:32:59 +0200

    feature: automatically create directories if not exist

--- backend/admin/src/app.ts
+++ backend/admin/src/app.ts
@@ -19,6 +19,16 @@ const start = async () => {
   const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
   app.use(express.static(path.join(__dirname, '../public'))); // path from dist to public
 
+  if (!fs.existsSync(`${process.env.ADMIN_DATA_DIRECTORY}/pathogen_example_data`)) {
+    fs.mkdirSync(`${process.env.ADMIN_DATA_DIRECTORY}/pathogen_example_data`);
+    console.log(`Pathogen example data directory was created.`);
+  }
+
+  if (!fs.existsSync(`${process.env.ADMIN_DATA_DIRECTORY}/pathogen_schemes`)) {
+    fs.mkdirSync(`${process.env.ADMIN_DATA_DIRECTORY}/pathogen_schemes`);
+    console.log(`Pathogen schemes directory was created.`);
+  }
+
   // Setup Prisma Client and register it in AdminJS
   await prisma.$connect();
 
