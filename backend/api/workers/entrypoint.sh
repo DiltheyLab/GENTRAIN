108cba2084af71ed3d448ef3caf2cd6570ca5fa2
benjoka <ben.kraeling@icloud.com>
2025-11-04 11:53:01 +0100

    resolve merge conflicts

---
+++ backend/api/workers/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm -rf /api/temp_data/
+rm -rf /api/logs/
+mkdir -p /api/logs/
+prisma generate --generator py_client
+if [[ $1 == "dev" ]]; then
+  supervisord -c /api/workers/supervisor/dev.conf
+fi
+
+if [[ $1 == "prod" ]]; then
+  supervisord -c /api/workers/supervisor/prod.conf
+fi
