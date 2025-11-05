6109590d93556c761b125040ff121c5668ac4fef
benjoka <ben.kraeling@study.hs-duesseldorf.de>
2025-11-05 13:50:11 +0100

    chore: finalize getting started docs

--- backend/api/workers/entrypoint.sh
+++ backend/api/workers/entrypoint.sh
@@ -2,7 +2,6 @@
 rm -rf /api/temp_data/
 rm -rf /api/logs/
 mkdir -p /api/logs/
-prisma generate --generator python_client
 if [[ $1 == "dev" ]]; then
   supervisord -c /api/workers/supervisor/dev.conf
 fi
