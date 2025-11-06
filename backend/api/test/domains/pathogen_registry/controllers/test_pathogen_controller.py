0077ee4579604808be05f20b966d0bfcb7ddba58
benjoka <ben.kraeling@study.hs-duesseldorf.de>
2025-11-06 10:31:10 +0100

    chore: add mocked controller test

--- backend/api/test/domains/pathogen_registry/controllers/test_pathogen_controller.py
+++ backend/api/test/domains/pathogen_registry/controllers/test_pathogen_controller.py
@@ -1,12 +1,45 @@
+import datetime
+
 import pytest
-from os import environ
-from prisma import Prisma
+from werkzeug.exceptions import NotFound
 
-@pytest.fixture
-async def prisma_client() -> Prisma:
-    prisma = Prisma()
-    yield await prisma.connect()
+from prisma.models import Pathogen
+from src.domains.pathogen_registry.controllers.pathogen_controller import get_pathogen_action
+from src.domains.pathogen_registry.resources import Pathogen as PathogenResource
 
-async def test_get_pathogen_action_return_200(prisma_client):
-    print("HI", prisma_client)
-    assert True
+
+def test_get_pathogen_action_returns_not_found_exception(mocker):
+    mock_find_unique = mocker.patch(
+        "prisma.models.Pathogen.prisma"
+    )
+    mock_find_unique.return_value.find_unique.return_value = None
+    with pytest.raises(NotFound):
+        get_pathogen_action(0)
+    mock_find_unique.return_value.find_unique.assert_called_once()
+
+
+def test_get_pathogen_action_returns_pathogen_resource(mocker):
+    pathogen = Pathogen(id=1, name='SARS-CoV-2',
+                        genetic_distance_threshold=1, type="viral",
+                        activated=True,
+                        scheme_version=datetime.datetime(2025, 11, 6, 0,
+                                                         0, 0),
+                        scheme_size=1000)
+    mock_find_unique = mocker.patch(
+        "prisma.models.Pathogen.prisma"
+    )
+    mock_find_unique.return_value.find_unique.return_value = pathogen
+    response = get_pathogen_action(1)
+    pathogen_resource = PathogenResource(
+        id=pathogen.id,
+        name=pathogen.name,
+        scheme_version=pathogen.scheme_version,
+        type=pathogen.type,
+        activated=pathogen.activated,
+        genetic_distance_threshold=pathogen.genetic_distance_threshold,
+        cases_example=pathogen.example_cases_key,
+        contacts_example=pathogen.example_contacts_key,
+        sequences_example=pathogen.example_sequences_key,
+    )
+    assert response == pathogen_resource.model_dump()
+    mock_find_unique.return_value.find_unique.assert_called_once()
