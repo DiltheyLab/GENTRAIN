8a75d0358b7fc339922ddd583644b37d8a5d42e4
PhilippVogel92 <philipp242@live.de>
2025-11-11 10:12:58 +0100

    feat(admin): add seeding for tests

--- backend/admin/entrypoint.prod.sh
+++ backend/admin/entrypoint.prod.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+set -e
+
 npx prisma generate --generator js_client
 npx prisma migrate deploy
+
+if [ "$RUN_SEED" = "true" ]; then
+    npx prisma db seed
+fi
+
 supervisord -c /admin/supervisor.conf -n
