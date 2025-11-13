444b4bb53460b4eeb564cf2a11d174a89cfbfc82
benjoka <ben.kraeling@study.hs-duesseldorf.de>
2025-11-13 10:56:04 +0100

    add fixture for pathogen resource

--- backend/api/test/domains/pathogen_registry/controllers/test_pathogen_controller.py
+++ backend/api/test/domains/pathogen_registry/controllers/test_pathogen_controller.py
@@ -8,6 +8,20 @@ from src.domains.pathogen_registry.controllers.pathogen_controller import get_pa
 from src.domains.pathogen_registry.resources import Pathogen as PathogenResource
 
 
+@pytest.fixture
+def make_pathogen_resource():
+    def _make_pathogen_resource(id=1, name=":name:", genetic_distance_threshold=1, type="viral", activated=True,
+                                scheme_version=datetime.datetime(2025, 11, 6, 0,
+                                                                 0, 0), scheme_size=1000):
+        return Pathogen(id=id, name=name,
+                        genetic_distance_threshold=genetic_distance_threshold, type=type,
+                        activated=activated,
+                        scheme_version=scheme_version,
+                        scheme_size=scheme_size)
+
+    return _make_pathogen_resource
+
+
 def test_get_all_pathogen_action_returns_empty_list(mocker):
     mock_find_unique = mocker.patch(
         "prisma.models.Pathogen.prisma"
@@ -18,26 +32,10 @@ def test_get_all_pathogen_action_returns_empty_list(mocker):
     mock_find_unique.return_value.find_many.assert_called_once()
 
 
-def test_get_all_pathogen_action_returns_pathogen_resource_list(mocker):
-    pathogens = [Pathogen(id=1, name=':pathogen_1:',
-                          genetic_distance_threshold=1, type="viral",
-                          activated=True,
-                          scheme_version=datetime.datetime(2025, 11, 6, 0,
-                                                           0, 0),
-                          scheme_size=1000),
-                 Pathogen(id=2, name=':pathogen_2:',
-                          genetic_distance_threshold=1, type="viral",
-                          activated=True,
-                          scheme_version=datetime.datetime(2025, 11, 7, 1,
-                                                           1, 1),
-                          scheme_size=1000),
-                 Pathogen(id=3, name=':pathogen_3:',
-                          genetic_distance_threshold=1, type="bacterial",
-                          activated=True,
-                          scheme_version=datetime.datetime(2025, 11, 7, 1,
-                                                           1, 1),
-                          scheme_size=1000)
-                 ]
+def test_get_all_pathogen_action_returns_pathogen_resource_list(mocker, make_pathogen_resource):
+    pathogens = [make_pathogen_resource(id=1, name=":pathogen_1:", type="viral"),
+                 make_pathogen_resource(id=2, name=":pathogen_2:", type="viral"),
+                 make_pathogen_resource(id=3, name=":pathogen_3:", type="bacterial")]
     mock_find_unique = mocker.patch(
         "prisma.models.Pathogen.prisma"
     )
@@ -68,13 +66,8 @@ def test_get_pathogen_action_returns_not_found_exception(mocker):
     mock_find_unique.return_value.find_unique.assert_called_once()
 
 
-def test_get_pathogen_action_returns_pathogen_resource(mocker):
-    pathogen = Pathogen(id=1, name='SARS-CoV-2',
-                        genetic_distance_threshold=1, type="viral",
-                        activated=True,
-                        scheme_version=datetime.datetime(2025, 11, 6, 0,
-                                                         0, 0),
-                        scheme_size=1000)
+def test_get_pathogen_action_returns_single_pathogen_resource(mocker, make_pathogen_resource):
+    pathogen = make_pathogen_resource()
     mock_find_unique = mocker.patch(
         "prisma.models.Pathogen.prisma"
     )
