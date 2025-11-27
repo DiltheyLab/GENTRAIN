ac8a07bf713fd406b450f6f61f5c58c16dbebe2a
benjoka <ben.kraeling@study.hs-duesseldorf.de>
2025-11-27 13:19:32 +0100

    fix: return correct alignment

--- backend/api/src/domains/sequence_analysis/controllers/sequence_controller.py
+++ backend/api/src/domains/sequence_analysis/controllers/sequence_controller.py
@@ -1,6 +1,7 @@
 from Bio import Align
 from flask import Response, jsonify
 
+
 def align_sequences_action(data):
     if "sequence_1" not in data or "sequence_2" not in data:
         return Response(
@@ -9,9 +10,16 @@ def align_sequences_action(data):
     sequence_1 = data["sequence_1"]
     sequence_2 = data["sequence_2"]
 
+    if sequence_1 == sequence_2:
+        return jsonify(
+            {
+                "aligned_sequence_1": sequence_1,
+                "aligned_sequence_2": sequence_2,
+            }
+        )
+
     aligner = Align.PairwiseAligner(match_score=1.0)
     alignments = aligner.align(sequence_1, sequence_2)
-
     return jsonify(
-        {"aligned_sequence_1": alignments[0][0], "aligned_sequence_2": alignments[1][1]}
+        {"aligned_sequence_1": alignments[0][0], "aligned_sequence_2": alignments[0][1]}
     )
