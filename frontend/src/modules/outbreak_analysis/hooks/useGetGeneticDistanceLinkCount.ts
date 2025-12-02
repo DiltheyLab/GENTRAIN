1f8f74612f3be11dc810e8a808d0989a4cef5c91
PhilippVogel92 <philipp242@live.de>
2025-12-02 16:09:02 +0100

    refactor: simplify useGetGeneticDistanceLinkCount by removing state and effect hooks

--- frontend/src/modules/outbreak_analysis/hooks/useGetGeneticDistanceLinkCount.ts
+++ frontend/src/modules/outbreak_analysis/hooks/useGetGeneticDistanceLinkCount.ts
@@ -1,15 +1,13 @@
-import { useEffect, useState } from "react";
+import { useMemo } from "react";
 import { useOutbreakAnalysisStore } from "../stores/outbreakAnalysis";
 import { useTranslation } from "react-i18next";
 
 export const useGetGeneticDistanceLinkCount = () => {
     const { links } = useOutbreakAnalysisStore((state) => state.graphData);
     const { t } = useTranslation();
-    const [geneticDistanceLinkCount, setGeneticDistanceLinkCount] = useState(0);
+    const geneticDistanceType = t("linkTypes.geneticDistance");
 
-    useEffect(() => {
-        setGeneticDistanceLinkCount(links.filter((link) => link.type === t("linkTypes.geneticDistance")).length)
-    }, [links]);
-
-    return geneticDistanceLinkCount;
+    return useMemo(() => {
+        return links.filter((link) => link.type === geneticDistanceType).length;
+    }, [links, geneticDistanceType]);
 };
